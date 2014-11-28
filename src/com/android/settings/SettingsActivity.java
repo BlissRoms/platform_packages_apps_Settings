e2581cc97987f905c42e5f2991b41879dba6aa6f
Jorrit Jongma <git@jongma.org>
2014-11-28 15:03:11 +0100

    Launch SuperSU from Settings
    If SuperSU is installed, show an entry in Settings below
    "Developer options", which launches SuperSU when tapped.
    At boot time SuperSU will detect if com.android.settings
    contains the resources from this patch, and if so it
    will hide itself from the launcher. This was implemented
    in v1.85, so we only show the entry if the installed
    SuperSU version is at least that.
    
    PS3 - Added material green icons for SU
    
    Change-Id: I398b3db8e11f6de05b154e49c2100d2bdec8cdfc

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -1335,6 +1335,16 @@ public class SettingsActivity extends Activity
                 } else if (id == R.id.weather_settings) {
                     if (!getPackageManager().hasSystemFeature(
                             CMContextConstants.Features.WEATHER_SERVICES)) {
+                         removeTile = true;
+                    }
+                } else if (id == R.id.supersu_settings) {
+                    // Embedding into Settings is supported from SuperSU v1.85 and up
+                    boolean supported = false;
+                    try {
+                        supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
+                    } catch (PackageManager.NameNotFoundException e) {
+                    }
+                    if (!supported) {
                         removeTile = true;
                     }
                 } else if (id == R.id.kernel_adiutor) {
@@ -1382,7 +1392,7 @@ public class SettingsActivity extends Activity
         }
         addExternalTiles(target);
     }
-
+ 
     private void addExternalTiles(List<DashboardCategory> target) {
         Map<Pair<String, String>, DashboardTile> addedCache =
                 new ArrayMap<Pair<String, String>, DashboardTile>();
