97a56e049fb3d5de8cd8de47820634432a02bc69
Andrew Boren <andrew.boren@gmail.com>
2015-01-25 09:22:19 +0100

    Powermenu screenshot delay [2/2]
    Change-Id: I07ec9ba89fb1b36adcd2daecb90f0d20907ef063

--- src/com/android/settings/cyanogenmod/PowerMenuActions.java
+++ src/com/android/settings/cyanogenmod/PowerMenuActions.java
@@ -17,6 +17,7 @@
 package com.android.settings.cyanogenmod;
 
 import android.content.Context;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -41,12 +42,15 @@ import cyanogenmod.providers.CMSettings;
 import org.cyanogenmod.internal.logging.CMMetricsLogger;
 
 import static com.android.internal.util.cm.PowerMenuConstants.*;
+import com.android.settings.widget.NumberPickerPreference;
 
 import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PowerMenuActions extends SettingsPreferenceFragment {
+public class PowerMenuActions extends SettingsPreferenceFragment
+        implements OnPreferenceChangeListener {
+
     final static String TAG = "PowerMenuActions";
 
     private CheckBoxPreference mRebootPref;
@@ -59,10 +63,22 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     private CheckBoxPreference mVoiceAssistPref;
     private CheckBoxPreference mAssistPref;
 
+    private static final String SCREENSHOT_DELAY = "screenshot_delay";
+
     Context mContext;
     private ArrayList<String> mLocalUserConfig = new ArrayList<String>();
     private String[] mAvailableActions;
     private String[] mAllActions;
+
+
+    private NumberPickerPreference mScreenshotDelay;
+
+    private ContentResolver mCr;
+    private PreferenceScreen mPrefSet;
+
+    private static final int MIN_DELAY_VALUE = 1;
+    private static final int MAX_DELAY_VALUE = 30;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -70,6 +86,11 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
 
         addPreferencesFromResource(R.xml.power_menu_settings);
         mContext = getActivity().getApplicationContext();
+
+        mPrefSet = getPreferenceScreen();
+
+        mCr = getActivity().getContentResolver();
+
 
         mAvailableActions = getActivity().getResources().getStringArray(
                 R.array.power_menu_actions_array);
@@ -102,6 +123,15 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
                 mSilentPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_ASSIST);
             }
         }
+        mScreenshotDelay = (NumberPickerPreference) mPrefSet.findPreference(
+                SCREENSHOT_DELAY);
+        mScreenshotDelay.setOnPreferenceChangeListener(this);
+        mScreenshotDelay.setMinValue(MIN_DELAY_VALUE);
+        mScreenshotDelay.setMaxValue(MAX_DELAY_VALUE);
+        int ssDelay = Settings.System.getInt(mCr,
+                Settings.System.SCREENSHOT_DELAY, 1);
+        mScreenshotDelay.setCurrentValue(ssDelay);
+
 
         getUserConfig();
     }
@@ -213,6 +243,18 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         return true;
     }
 
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference == mScreenshotDelay) {
+            int value = Integer.parseInt(newValue.toString());
+            Settings.System.putInt(mCr, Settings.System.SCREENSHOT_DELAY,
+                    value);
+            return true;
+        }
+        return false;
+    }
+
+
     private boolean settingsArrayContains(String preference) {
         return mLocalUserConfig.contains(preference);
     }
@@ -287,3 +329,4 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         mContext.sendBroadcastAsUser(u, UserHandle.ALL);
     }
 }
+
