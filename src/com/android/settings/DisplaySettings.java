8d149e7b2c2408a3f11eacea063ce9b92b05eb3c
Jackeagle <jackeagle102@gmail.com>
2015-05-03 23:21:55 +0530

    Add Blissify into Settings
    - Add Blissify icon
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    
    Conflicts:
    	Android.mk
    	res/values/bliss_strings.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java
    
    Change-Id: Ic9e893f31884ebd6781f2fa8cbca28e3884fc8f5

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -121,6 +121,11 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final String KEY_WAKEUP_CATEGORY = "category_wakeup_options";
     private static final String KEY_VOLUME_ROCKER_WAKE = "volume_rocker_wake";
 
+    private static final String KEY_NETWORK_NAME_DISPLAYED = "network_operator_display";
+    private static final String SHOW_NETWORK_NAME_MODE = "show_network_name_mode";
+    private static final int SHOW_NETWORK_NAME_ON = 1;
+    private static final int SHOW_NETWORK_NAME_OFF = 0;
+
     private static final String FONT_SIZE_MINIMUM = "0.95";
     private static final String FONT_SIZE_SMALL = "1.0";
     private static final String FONT_SIZE_MEDIUM = "1.05";
