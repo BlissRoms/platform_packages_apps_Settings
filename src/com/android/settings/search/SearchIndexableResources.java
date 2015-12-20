ResurrectionRemix/Resurrection_packages_apps_Settings

settings: fix build
Signed-off-by: Akhil Narang <akhilnarang.1999@gmail.com>

src/com/android/settings/search/SearchIndexableResources.java
@@ -97,7 +97,7 @@ public final class SearchIndexableResources {
                         Ranking.getRankForClassName(MainSettings.class.getName()),
                         NO_DATA_RES_ID,
                         MainSettings.class.getName(),
-                        R.drawable.ic_settings_interface));
+                        R.drawable.ic_ic_settings_about_rr));
         sResMap.put(SimSettings.class.getName(),
                 new SearchIndexableResource(
                         Ranking.getRankForClassName(SimSettings.class.getName()),

BlissRoms/platform_packages_apps_Settings

Change-Id: I27b10cf8ae371b3afe185eac66d73c34a337e5ab

src/com/android/settings/search/SearchIndexableResources.java
@@ -98,7 +98,7 @@ public final class SearchIndexableResources {
                         Ranking.getRankForClassName(MainSettings.class.getName()),
                         NO_DATA_RES_ID,
                         MainSettings.class.getName(),
-                        R.drawable.ic_settings_interface));
+                        R.drawable.ic_settings_bliss));
         sResMap.put(SimSettings.class.getName(),
                 new SearchIndexableResource(
                         Ranking.getRankForClassName(SimSettings.class.getName()),