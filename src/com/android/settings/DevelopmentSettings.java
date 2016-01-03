7646e33bd9637aecc0ed5838e6dd02e5de1e9ee4
Dark Eyes <dark_eyes_@hotmail.com>
2016-01-02 23:45:14 -0500

    Change placement of some settings and fix a few things
    Change-Id: Iad0295b57430b5ac0e9ec6a90f6a2adf7990f465

--- src/com/android/settings/DevelopmentSettings.java
+++ src/com/android/settings/DevelopmentSettings.java
@@ -82,7 +82,6 @@ import com.android.settings.fuelgauge.InactiveApps;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
 import com.android.settings.widget.SwitchBar;
-import com.android.settings.widget.SeekBarPreferenceCham;
 import cyanogenmod.providers.CMSettings;
 
 import java.util.ArrayList;
@@ -180,9 +179,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
 
     private static final String SHOW_ALL_ANRS_KEY = "show_all_anrs";
 
-    private static final String KILL_APP_LONGPRESS_BACK = "kill_app_longpress_back";
-    private static final String HOLD_BACK_TO_KILL_TIMEOUT = "hold_back_to_kill_timeout";
-
     private static final String PACKAGE_MIME_TYPE = "application/vnd.android.package-archive";
 
     private static final String TERMINAL_APP_PACKAGE = "com.android.terminal";
@@ -275,8 +271,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
     private ListPreference mAppProcessLimit;
 
     private SwitchPreference mShowAllANRs;
-    private SwitchPreference mKillAppLongpressBack;
-    private SeekBarPreferenceCham mHoldBackToKillTimeout;
 
     private ListPreference mRootAccess;
     private Object mSelectedRootValue;
@@ -449,14 +443,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         mAllPrefs.add(mShowAllANRs);
         mResetSwitchPrefs.add(mShowAllANRs);
 
-        mKillAppLongpressBack = findAndInitSwitchPref(KILL_APP_LONGPRESS_BACK);
-        mHoldBackToKillTimeout =
-                    (SeekBarPreferenceCham) findPreference(HOLD_BACK_TO_KILL_TIMEOUT);
-        int holdBackToKillTimeout = Settings.System.getInt(getActivity().getContentResolver(),
-                Settings.System.HOLD_BACK_TO_KILL_TIMEOUT, 750);
-        mHoldBackToKillTimeout.setValue(holdBackToKillTimeout / 1);
-        mHoldBackToKillTimeout.setOnPreferenceChangeListener(this);
-
         Preference hdcpChecking = findPreference(HDCP_CHECKING_KEY);
         if (hdcpChecking != null) {
             mAllPrefs.add(hdcpChecking);
@@ -606,7 +592,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             setPrefsEnabledState(mLastEnabledState);
         }
         mSwitchBar.show();
-        updateKillAppLongpressBackOptions();
 
         if (mColorModePreference != null) {
             mColorModePreference.startListening();
@@ -880,17 +865,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             hdcpChecking.setSummary(summaries[index]);
             hdcpChecking.setOnPreferenceChangeListener(this);
         }
-    }
-
-    private void writeKillAppLongpressBackOptions() {
-        CMSettings.Secure.putInt(getActivity().getContentResolver(),
-                CMSettings.Secure.KILL_APP_LONGPRESS_BACK,
-                mKillAppLongpressBack.isChecked() ? 1 : 0);
-    }
-
-    private void updateKillAppLongpressBackOptions() {
-        mKillAppLongpressBack.setChecked(CMSettings.Secure.getInt(
-            getActivity().getContentResolver(), CMSettings.Secure.KILL_APP_LONGPRESS_BACK, 0) != 0);
     }
 
     private void updatePasswordSummary() {
@@ -1933,8 +1907,6 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
             startInactiveAppsFragment();
         } else if (preference == mDevelopmentShortcut) {
             writeDevelopmentShortcutOptions();
-        } else if (preference == mKillAppLongpressBack) {
-            writeKillAppLongpressBackOptions();
         } else {
             return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
@@ -2017,12 +1989,8 @@ public class DevelopmentSettings extends SettingsPreferenceFragment
         } else if (preference == mKeepScreenOn) {
             writeStayAwakeOptions(newValue);
             return true;
-        } else if (preference == mHoldBackToKillTimeout) {
-            int killTimeout = (Integer) newValue;
-            Settings.System.putInt(getActivity().getContentResolver(),
-                    Settings.System.HOLD_BACK_TO_KILL_TIMEOUT, killTimeout * 1);
-            return true;
         }
+        
         return false;
     }
 
