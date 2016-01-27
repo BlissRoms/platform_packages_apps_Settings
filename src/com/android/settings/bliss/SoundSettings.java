27d126a2c5b73f24700eb61f821b807c79b07110
Dark Eyes <dark_eyes_@hotmail.com>
2016-01-27 15:10:00 -0500

    Settings : safe headset volume switch (2/3)
    Change-Id: I669f625c0ad533f3c9fe601cd8e1ab8ca0f836e6

--- src/com/android/settings/bliss/SoundSettings.java
+++ src/com/android/settings/bliss/SoundSettings.java
@@ -23,6 +23,7 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.SystemProperties;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -33,19 +34,23 @@ import android.util.Log;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
-
 import com.android.internal.logging.MetricsLogger;
+
+import cyanogenmod.providers.CMSettings;
 
 public class SoundSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
     private static final String TAG = "SoundSettings";
 
+    private static final int DLG_SAFE_HEADSET_VOLUME = 0;
     private static final int DLG_CAMERA_SOUND = 1;
 
     private static final String KEY_CAMERA_SOUNDS = "camera_sounds";
     private static final String PROP_CAMERA_SOUND = "persist.sys.camera-sound";
     private static final String PREF_LESS_NOTIFICATION_SOUNDS = "less_notification_sounds";
+    private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
+    private SwitchPreference mSafeHeadsetVolume;
     private SwitchPreference mCameraSounds;
     private ListPreference mAnnoyingNotifications;
 
@@ -59,6 +64,11 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
 
         addPreferencesFromResource(R.xml.sound_settings);
+
+        mSafeHeadsetVolume = (SwitchPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
+        mSafeHeadsetVolume.setChecked(CMSettings.System.getInt(getContentResolver(),
+                CMSettings.System.SAFE_HEADSET_VOLUME, 1) != 0);
+        mSafeHeadsetVolume.setOnPreferenceChangeListener(this);
 
         mCameraSounds = (SwitchPreference) findPreference(KEY_CAMERA_SOUNDS);
         mCameraSounds.setChecked(SystemProperties.getBoolean(PROP_CAMERA_SOUND, true));
@@ -84,6 +94,14 @@ public class SoundSettings extends SettingsPreferenceFragment implements
 
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         final String key = preference.getKey();
+        if (KEY_SAFE_HEADSET_VOLUME.equals(key)) {
+            if ((Boolean) objValue) {
+                CMSettings.System.putInt(getContentResolver(),
+                        CMSettings.System.SAFE_HEADSET_VOLUME, 1);
+            } else {
+                showDialogInner(DLG_SAFE_HEADSET_VOLUME);
+            }
+        }
         if (KEY_CAMERA_SOUNDS.equals(key)) {
            if ((Boolean) objValue) {
                SystemProperties.set(PROP_CAMERA_SOUND, "1");
@@ -139,6 +157,24 @@ public class SoundSettings extends SettingsPreferenceFragment implements
                         }
                     })
                     .create();
+                case DLG_SAFE_HEADSET_VOLUME:
+                    return new AlertDialog.Builder(getActivity())
+                    .setTitle(R.string.attention)
+                    .setMessage(R.string.safe_headset_volume_warning_dialog_text)
+                    .setPositiveButton(R.string.dlg_ok,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            CMSettings.System.putInt(getOwner().getContentResolver(),
+                                    CMSettings.System.SAFE_HEADSET_VOLUME, 0);
+                        }
+                    })
+                    .setNegativeButton(R.string.dlg_cancel,
+                        new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    })
+                    .create();
             }
             throw new IllegalArgumentException("unknown id " + id);
         }
@@ -147,11 +183,13 @@ public class SoundSettings extends SettingsPreferenceFragment implements
         public void onCancel(DialogInterface dialog) {
             int id = getArguments().getInt("id");
             switch (id) {
+                case DLG_SAFE_HEADSET_VOLUME:
+                    getOwner().mSafeHeadsetVolume.setChecked(true);
+                    break;
                 case DLG_CAMERA_SOUND:
                     getOwner().mCameraSounds.setChecked(true);
                     break;
             }
         }
     }
-
 }
