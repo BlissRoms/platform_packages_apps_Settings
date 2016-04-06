sarthakspiker/packages_apps_Settings

Dashboard tileview double lines toggle [1/2]
Inspired by @xWASABI and his commit here
https://github.com/TeamTwisted/packages_apps_Settings/commit/3c62ed2f244ae21436e02556894c2f5b46365024

This would come in handy for tablets that use 2 rows in the dashboard and for when
translations are just too big to fit in 1 line

Change-Id: I488630c7d4e9fc8f0d93891480f61f087149c018
Signed-off-by: GrinningFerret <luis.hage@outlook.de>

Conflicts:
	res/values/du_strings.xml

src/com/android/settings/dashboard/DashboardTileView.java
@@ -18,6 +18,7 @@ package com.android.settings.dashboard;
 
 import android.app.Activity;
 import android.content.Context;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -52,7 +53,15 @@ public class DashboardTileView extends FrameLayout implements View.OnClickListen
         final View view = LayoutInflater.from(context).inflate(R.layout.dashboard_tile, this);
 
         mImageView = (ImageView) view.findViewById(R.id.icon);
+
         mTitleTextView = (TextView) view.findViewById(R.id.title);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.DASHBOARD_TILEVIEW_DOUBLE_LINES, 0) == 1) {
+        mTitleTextView.setSingleLine(false);
+        } else {
+        mTitleTextView.setSingleLine(true);
+        }
+
         mStatusTextView = (TextView) view.findViewById(R.id.status);
         mDivider = view.findViewById(R.id.tile_divider);
 

BlissRoms/platform_packages_apps_Settings

Change-Id: I59151e5fd4a1a36f2da32a107c59d540ca7f8dbc

src/com/android/settings/dashboard/DashboardTileView.java
@@ -18,6 +18,7 @@ package com.android.settings.dashboard;
 
 import android.app.Activity;
 import android.content.Context;
+import android.provider.Settings;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -58,7 +59,15 @@ public class DashboardTileView extends FrameLayout implements View.OnClickListen
         final View view = LayoutInflater.from(context).inflate(R.layout.dashboard_tile, this);
 
         mImageView = (ImageView) view.findViewById(R.id.icon);
+
         mTitleTextView = (TextView) view.findViewById(R.id.title);
+        if (Settings.System.getInt(mContext.getContentResolver(),
+                Settings.System.DASHBOARD_TWO_COLUMNS, 0) == 1) {
+            mTitleTextView.setSingleLine(false);
+        } else {
+            mTitleTextView.setSingleLine(true);
+        }
+
         mStatusTextView = (TextView) view.findViewById(R.id.status);
         mDivider = view.findViewById(R.id.tile_divider);
         mSwitch = (Switch) view.findViewById(R.id.dashboard_switch);