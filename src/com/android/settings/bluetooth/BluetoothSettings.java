bf79d8b62c44b649027304ec864d29cc35b36478
Qiang Wu <qiang.x.wu@sonymobile.com>
2016-08-30 13:50:30 +0800

    Fix two same BT devices are shown
    If a cached device is not found, the paired device is added
    to PreferenceScreen twice when Bluetooth is turned on.
    To fix this issue, all Preferences should be removed once
    when Bluetooth state is changed to ON.
    
    Bug: 31506074
    
    Change-Id: I8c05caa3bb93b1a209715e1197adfdd01708dc52
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>

--- src/com/android/settings/bluetooth/BluetoothSettings.java
+++ src/com/android/settings/bluetooth/BluetoothSettings.java
@@ -315,6 +315,7 @@ public final class BluetoothSettings extends DeviceListPreferenceFragment implem
 
         switch (bluetoothState) {
             case BluetoothAdapter.STATE_ON:
+                preferenceScreen.removeAll();
                 mDevicePreferenceMap.clear();
 
                 if (isUiRestricted()) {
