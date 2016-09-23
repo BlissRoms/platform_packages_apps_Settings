6a3bf9c07f0dbd0a7b1f05d5d13dfa1e48552467
Yasir <saeedyasir91@gmail.com>
2016-09-23 14:03:13 -0400

    Utils: add deviceSupportsFlashLight boolean
    Change-Id: I53f48eac7b69fe8396838d2ff98a2845d526d548

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -39,6 +39,9 @@ import android.content.pm.IntentFilterVerificationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraManager;
 import android.content.pm.ResolveInfo;
 import android.content.pm.UserInfo;
 import android.content.res.Resources;
@@ -1277,6 +1280,28 @@ public final class Utils extends com.android.settingslib.Utils {
                     " with extra " + extra + ", " + extraVal);
             context.getApplicationContext().sendBroadcast(intent);
         }
+    }
+
+    public static boolean deviceSupportsFlashLight(Context context) {
+        CameraManager cameraManager = (CameraManager) context.getSystemService(
+                Context.CAMERA_SERVICE);
+        try {
+            String[] ids = cameraManager.getCameraIdList();
+            for (String id : ids) {
+                CameraCharacteristics c = cameraManager.getCameraCharacteristics(id);
+                Boolean flashAvailable = c.get(CameraCharacteristics.FLASH_INFO_AVAILABLE);
+                Integer lensFacing = c.get(CameraCharacteristics.LENS_FACING);
+                if (flashAvailable != null
+                        && flashAvailable
+                        && lensFacing != null
+                        && lensFacing == CameraCharacteristics.LENS_FACING_BACK) {
+                    return true;
+                }
+            }
+        } catch (CameraAccessException e) {
+            // Ignore
+        }
+        return false;
     }
 
     public static boolean isPackageDirectBootAware(Context context, String packageName) {
