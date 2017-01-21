99f77ebb0ae5b73311f185cb1a765130fecd49dc
maxwen <max.weninger@gmail.com>
2017-01-21 10:00:31 +0100

    Dashboard tile layout config [1/2]
    add config to hide summary and use 2 columns
    always show one more column in landscape mode
    
    *DU Edits*
    
    - Replaced menu option to how we had it in M
    https://github.com/DirtyUnicorns/android_packages_apps_Settings/commit/8bfc3003f6ab45f28b0df96546dada0c88c19331
    
    - Added back integer so Substratum themers can overwrite this with themes
    
    Change-Id: I9a0f7354880b211f96f7fbbab99c3ae879f5b23d

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -64,6 +64,7 @@ import com.android.internal.logging.MetricsProto.MetricsEvent;
 import com.android.internal.view.RotationPolicy;
 import com.android.settings.accessibility.ToggleFontSizePreferenceFragment;
 import com.android.settings.dashboard.SummaryLoader;
+import com.android.settings.dashboard.DashboardSummary;
 import com.android.settings.display.ScreenZoomPreference;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.search.Indexable;
@@ -144,6 +145,8 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
     private static final int ROTATION_180_MODE = 4;
     private static final int ROTATION_270_MODE = 8;
 
+    private static final String DASHBOARD_COLUMNS = "dashboard_columns";
+
     private Preference mFontSizePref;
     private ScreenZoomPreference mScreenZoomPref;
     private WarnedPreference mDialogPref;
@@ -156,6 +159,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
 
     private ListPreference mScreenshotType;
     private ListPreference mNightModePreference;
+    private ListPreference mDashboardColumns;
     private Preference mScreenSaverPreference;
     private SwitchPreference mLiftToWakePreference;
     private SwitchPreference mDozePreference;
@@ -283,6 +287,12 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         } else {
             removePreference(KEY_ROTATION_CATEGORY);
         }
+
+        mDashboardColumns = (ListPreference) findPreference(DASHBOARD_COLUMNS);
+        mDashboardColumns.setValue(String.valueOf(Settings.System.getInt(
+                getContentResolver(), Settings.System.DASHBOARD_COLUMNS, DashboardSummary.mNumColumns)));
+        mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+        mDashboardColumns.setOnPreferenceChangeListener(this);
 
         if (isVrDisplayModeAvailable(activity)) {
             DropDownPreference vrDisplayPref =
@@ -707,6 +717,13 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             boolean value = (Boolean) objValue;
             Settings.Secure.putInt(getContentResolver(), CAMERA_GESTURE_DISABLED,
                     value ? 0 : 1 /* Backwards because setting is for disabling */);
+        }
+        if (preference == mDashboardColumns) {
+            Settings.System.putInt(getContentResolver(), Settings.System.DASHBOARD_COLUMNS,
+                    Integer.valueOf((String) objValue));
+            mDashboardColumns.setValue(String.valueOf(objValue));
+            mDashboardColumns.setSummary(mDashboardColumns.getEntry());
+            return true;
         }
         if (preference == mNightModePreference) {
             try {
