94ecb4f391aaf0424b981e031f9ae0e59a79b640
Ali B <abittin@gmail.com>
2017-01-22 17:12:20 +0300

    Dashboard customizations
    Thanks to DU and xyyx@mail.ru
    
    Squashed commit of
    b0854f8ede942a0fea3e1625bc64c31c1c2c8546 Remove dashboard tile summaries [1/2]
    10577ad032ca6af7a4c37bad435406ce73cf57a8 Disable dashboard conditions [2/2]
    
    Change-Id: I9588f815ade1cf968820632444088209b70be26f
    Signed-off-by: Varun Date <date.varun123@gmail.com>

--- src/com/android/settings/dashboard/DashboardAdapter.java
+++ src/com/android/settings/dashboard/DashboardAdapter.java
@@ -133,13 +133,12 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         if ((Settings.System.getInt(mContext.getContentResolver(),
                     Settings.System.DISABLE_SUGGESTIONS, 1) == 1)) {
         mSuggestions = suggestions;
-   } else {
+    } else {
         mSuggestions = null;
-   }
+    }
         mCategories = categories;
         recountItems();
     }
-
     public Tile getTile(ComponentName component) {
         for (int i = 0; i < mCategories.size(); i++) {
             for (int j = 0; j < mCategories.get(i).tiles.size(); j++) {
@@ -452,8 +451,13 @@ public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.Dash
         holder.icon.setImageDrawable(mCache.getIcon(tile.icon));
         holder.title.setText(tile.title);
         if (!TextUtils.isEmpty(tile.summary)) {
-            holder.summary.setText(tile.summary);
-            holder.summary.setVisibility(View.VISIBLE);
+           if ((Settings.System.getInt(mContext.getContentResolver(),
+                    Settings.System.REMOVE_TILE_SUMMARY, 0) == 1)) {
+                holder.summary.setVisibility(View.GONE);
+            } else {
+                holder.summary.setText(tile.summary);
+                holder.summary.setVisibility(View.VISIBLE);
+            }
         } else {
             holder.summary.setVisibility(View.GONE);
         }
