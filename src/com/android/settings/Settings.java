cfc2b3658c736052709f2dd742860c094e9a9dad
ezio84 <brabus84@gmail.com>
2017-02-03 00:26:51 +0100

    Settings: Allow to quickly open Running Services from meminfo view
    @neobuddy89: Updated for A14.
    
    Change-Id: I252ce85bda3daab5c5e4db097a6fd743365bc309
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -524,4 +524,6 @@ public class Settings extends SettingsActivity {
     public static class ContactsStorageSettingsActivity extends SettingsActivity { /* empty */ }
 
     public static class BlissifyActivity extends SettingsActivity {}
+
+    public static class DevRunningServicesActivity extends SettingsActivity { /* empty */ }
 }
--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -46,6 +46,7 @@ import com.android.settings.applications.AppDashboardFragment;
 import com.android.settings.applications.AppStorageSettings;
 import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
+import com.android.settings.applications.RunningServices;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.appcompat.UserAspectRatioDetails;
 import com.android.settings.applications.appinfo.AlarmsAndRemindersDetails;
@@ -414,7 +415,8 @@ public class SettingsGateway {
             ZenModesListFragment.class.getName(),
             ZenModeFragment.class.getName(),
             ContactsStorageSettings.class.getName(),
-            Blissify.class.getName()
+            Blissify.class.getName(),
+            RunningServices.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -462,5 +464,6 @@ public class SettingsGateway {
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
             Settings.BlissifyActivity.class.getName(),
+            Settings.DevRunningServicesActivity.class.getName(),
     };
 }
