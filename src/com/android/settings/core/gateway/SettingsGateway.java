29152159749a50241ca92812bf57270f5b24f8f5
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate Blissify into Settings
    - Adapt to Android U Changes
    - Keep it below Apps
    
    Change-Id: I30a65c3e17aff251eef6bbf2454203bbed3300ae
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/core/gateway/SettingsGateway.java
+++ src/com/android/settings/core/gateway/SettingsGateway.java
@@ -205,6 +205,8 @@ import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSettings2;
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
+import org.blissroms.blissify.Blissify;
+ 
 public class SettingsGateway {
 
     /**
@@ -398,6 +400,7 @@ public class SettingsGateway {
             CellularSecuritySettingsFragment.class.getName(),
             AccessibilityHearingAidsFragment.class.getName(),
             HearingDevicePairingFragment.class.getName(),
+            Blissify.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -444,5 +447,6 @@ public class SettingsGateway {
             UserBackupSettingsActivity.class.getName(),
             Settings.MemtagPageActivity.class.getName(),
             Settings.NavigationModeSettingsActivity.class.getName(),
+            Settings.BlissifySettingsActivity.class.getName(),
     };
 }
