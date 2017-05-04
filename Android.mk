9ba1db915965ac7149667182f459b5474505a46e
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate Blissify into Settings
    - Adapt to Android Q Changes
    - Keep it below Connected Devices
    
    Change-Id: I30a65c3e17aff251eef6bbf2454203bbed3300ae
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- Android.mk
+++ Android.mk
@@ -21,6 +21,7 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_USE_AAPT2 := true
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
+LOCAL_SRC_FILES += $(call all-java-files-under, ../Blissify/src) 
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx-constraintlayout_constraintlayout \
@@ -53,7 +54,13 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     zxing-core-1.7 \
     org.lineageos.platform.internal
 
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res \
+    packages/apps/Blissify/res
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
+
+LOCAL_AAPT_FLAGS := --auto-add-overlay \
+    --extra-packages com.blissroms.blissify
 
 ifneq ($(INCREMENTAL_BUILDS),)
     LOCAL_PROGUARD_ENABLED := disabled
