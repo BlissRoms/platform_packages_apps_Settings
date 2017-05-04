2e0f3f0f9b71fb84f5b852944dbdadfd7c077f92
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate Blissify into Settings
    - Adapt to Android S Changes
    - Keep it below Apps
    
    Change-Id: I30a65c3e17aff251eef6bbf2454203bbed3300ae
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -312,6 +312,7 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class BlissifySettingsActivity extends SettingsActivity {}
 
     /**
      * Activity for MediaControlsSettings
