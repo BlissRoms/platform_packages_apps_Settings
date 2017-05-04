4184be967f7840ce537beec7fbc53a2051a8d292
Jackeagle <jackeagle102@gmail.com>
2017-05-04 13:36:21 -0400

    Settings: Integrate Blissify into Settings
    Change-Id: I30a65c3e17aff251eef6bbf2454203bbed3300ae
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -176,5 +176,6 @@ public class Settings extends SettingsActivity {
     public static class StorageDashboardActivity extends SettingsActivity {}
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
+    public static class BlissifySettingsActivity extends SettingsActivity {}
 
 }
