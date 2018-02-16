c4e28ea87f894907c4247d9da6e226649913e6b1
Alex Cruz <du.alexcruz@gmail.com>
2018-02-15 20:32:51 -0500

    Ensure everything plays nice
    - Make system accent/theme selectors unavailable as well when substratum is detected
    - Make use of the methods in our Utils/fwb
    
    Change-Id: Ied7ddbeb33e427461c78bf14565c7bfb30468ee0
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/settings/display/AccentPickerPreferenceController.java
+++ src/com/android/settings/display/AccentPickerPreferenceController.java
@@ -30,12 +30,16 @@ import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.core.lifecycle.LifecycleObserver;
 import com.android.settingslib.core.lifecycle.events.OnResume;
 
-import com.aosip.owlsnest.AccentPicker;
+import org.candy.candyshop.fragments.AccentPicker;
+
+import com.android.internal.util.candy.CandyUtils;
 
 public class AccentPickerPreferenceController extends AbstractPreferenceController
         implements PreferenceControllerMixin, LifecycleObserver, OnResume {
 
     private static final String KEY_ACCENT_PICKER_FRAGMENT_PREF = "accent_picker";
+    private static final String SUBS_PACKAGE = "projekt.substratum";
+
     private static final int MY_USER_ID = UserHandle.myUserId();
 
     private final Fragment mParent;
@@ -52,6 +56,11 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
     @Override
     public void displayPreference(PreferenceScreen screen) {
         mAccentPickerPref  = (Preference) screen.findPreference(KEY_ACCENT_PICKER_FRAGMENT_PREF);
+        if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+            mAccentPickerPref.setEnabled(true);
+        } else {
+            mAccentPickerPref.setEnabled(false);
+        }
     }
 
     @Override
@@ -79,16 +88,25 @@ public class AccentPickerPreferenceController extends AbstractPreferenceControll
             new OnPreferenceClickListener() {
                 @Override
                 public boolean onPreferenceClick(Preference preference) {
-                    AccentPicker.show(mParent);
-                    return true;
+                   if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
+                        AccentPicker.show(mParent);
+                        return true;
+                   } else {
+                        return false;
+                   }
                 }
             });
     }
 
     public void updateSummary() {
         if (mAccentPickerPref != null) {
+            if (!CandyUtils.isPackageInstalled(mContext, SUBS_PACKAGE)) {
                 mAccentPickerPref.setSummary(mContext.getString(
                         com.android.settings.R.string.theme_accent_picker_summary));
+            } else {
+                mAccentPickerPref.setSummary(mContext.getString(
+                        com.android.settings.R.string.disable_accents_installed_title));
+            }
         }
     }
 }
