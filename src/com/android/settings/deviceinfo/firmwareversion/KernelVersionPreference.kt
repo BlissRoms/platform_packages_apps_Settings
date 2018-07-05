929237e77c7378521c47c951cec543e80624f5de
Alex <alex@dirtyunicorns.com>
2018-07-05 16:54:29 -0500

    Settings: Change kernel preference behavior
    Before you could only click once to see the full kernel version and if
    you wanted it to go back to the short version, you had to exit the fragment.
    
    Now you can click between the two (short and long).
    
    Change-Id: I6f89d6b145d5cc7b9cca6febc3651904b98da80c
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreference.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/KernelVersionPreference.kt
@@ -17,6 +17,7 @@
 package com.android.settings.deviceinfo.firmwareversion
 
 import android.content.Context
+import android.util.Log
 import androidx.preference.Preference
 import com.android.settings.R
 import com.android.settingslib.DeviceInfoUtils
@@ -26,12 +27,22 @@ import com.android.settingslib.metadata.PreferenceMetadata
 import com.android.settingslib.metadata.PreferenceSummaryProvider
 import com.android.settingslib.metadata.SensitivityLevel
 import com.android.settingslib.preference.PreferenceBinding
+import java.io.BufferedReader
+import java.io.FileReader
+import java.io.IOException
 
 // LINT.IfChange
-class KernelVersionPreference : PersistentPreference<String>, PreferenceMetadata, PreferenceSummaryProvider, PreferenceBinding {
+class KernelVersionPreference :
+    PersistentPreference<String>,
+    PreferenceMetadata,
+    PreferenceSummaryProvider,
+    PreferenceBinding,
+    Preference.OnPreferenceClickListener {
+
+    private var fullKernelVersion = false
 
     override val key: String
-        get() = "kernel_version"
+        get() = KEY
 
     override val purpose: Int
         get() = R.string.kernel_version_purpose
@@ -50,12 +61,39 @@ class KernelVersionPreference : PersistentPreference<String>, PreferenceMetadata
 
     override fun bind(preference: Preference, metadata: PreferenceMetadata) {
         super.bind(preference, metadata)
-        preference.isSelectable = false
         preference.isCopyingEnabled = true
+        preference.onPreferenceClickListener = this
+    }
+
+    override fun onPreferenceClick(preference: Preference): Boolean {
+        if (fullKernelVersion) {
+            preference.summary = DeviceInfoUtils.getFormattedKernelVersion(preference.context)
+            fullKernelVersion = false
+        } else {
+            preference.summary = getFullKernelVersion()
+            fullKernelVersion = true
+        }
+        return true
+    }
+
+    private fun getFullKernelVersion(): String {
+        return try {
+            BufferedReader(FileReader(FILENAME_PROC_VERSION), 256).use { it.readLine() }
+                ?: "Unavailable"
+        } catch (e: IOException) {
+            Log.e(LOG_TAG, "IO Exception when getting kernel version for Device Info screen", e)
+            "Unavailable"
+        }
     }
 
     override val sensitivityLevel
         get() = SensitivityLevel.NO_SENSITIVITY
+
+    companion object {
+        const val KEY = "kernel_version"
+        private const val FILENAME_PROC_VERSION = "/proc/version"
+        private const val LOG_TAG = "KernelVersionPreference"
+    }
 
 }
 // LINT.ThenChange(KernelVersionPreferenceController.java)
