970d220f45d0be05d5164854b83574a84b76ccd5
maxwen <max.weninger@gmail.com>
2018-08-19 13:58:44 +0200

    [2/2] Settings: Add style settings page
    Change-Id: I49a0e41ba43ac687bbf29ffcdcbb0f776a67e284

--- src/com/android/settings/search/SearchIndexableResourcesImpl.java
+++ src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -104,6 +104,7 @@ import java.util.Set;
 
 import org.omnirom.omnigears.OmniDashboardFragment;
 import org.omnirom.omnigears.ButtonSettings;
+import org.omnirom.omnigears.interfacesettings.StyleSettings;
 import org.omnirom.omnigears.moresettings.MoreSettings;
 
 public class SearchIndexableResourcesImpl implements SearchIndexableResources {
@@ -199,6 +200,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(OmniDashboardFragment.class);
         addIndex(ButtonSettings.class);
         addIndex(MoreSettings.class);
+        addIndex(StyleSettings.class);
     }
 
     @Override
