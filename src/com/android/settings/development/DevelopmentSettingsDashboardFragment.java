638fadb5fe42e8480e39ae51962244e385e5674e
maxwen <max.weninger@gmail.com>
2018-08-20 11:38:23 +0200

    Settings: Disable all bugreport settings
    we dont need those
    
    Change-Id: I7dd62da21e7a48b6a8182df60ac1a06461a1fa8e
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
+++ src/com/android/settings/development/DevelopmentSettingsDashboardFragment.java
@@ -512,8 +512,8 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new MemoryUsagePreferenceController(context));
         controllers.add(new EnableWebAppMinterPreferenceController(context));
-        controllers.add(new BugReportPreferenceController(context));
-        controllers.add(new BugReportHandlerPreferenceController(context));
+        //controllers.add(new BugReportPreferenceController(context));
+        //controllers.add(new BugReportHandlerPreferenceController(context));
         controllers.add(new SystemServerHeapDumpPreferenceController(context));
         controllers.add(new DevelopmentMemtagPagePreferenceController(context));
         controllers.add(new LocalBackupPasswordPreferenceController(context));
@@ -536,7 +536,7 @@ public class DevelopmentSettingsDashboardFragment extends RestrictedDashboardFra
         controllers.add(new AdbAuthorizationTimeoutPreferenceController(context));
         controllers.add(new LocalTerminalPreferenceController(context));
         controllers.add(new LinuxTerminalPreferenceController(context));
-        controllers.add(new BugReportInPowerPreferenceController(context));
+        //controllers.add(new BugReportInPowerPreferenceController(context));
         controllers.add(new AutomaticSystemServerHeapDumpPreferenceController(context));
         controllers.add(new MockLocationAppPreferenceController(context, fragment));
         controllers.add(new MockModemPreferenceController(context));
