9bfd76867589f5c15a798d5f196a9f11e5347e95
Joe Maples <joe@frap129.org>
2018-08-31 09:11:54 -0400

    fuelgauge: Fix possible memory leaks
    Change-Id: I6820941f16898f03e7b1145c0a8363dcdca2e492
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImpl.java
+++ src/com/android/settings/fuelgauge/PowerUsageFeatureProviderImpl.java
@@ -149,8 +149,15 @@ public class PowerUsageFeatureProviderImpl implements PowerUsageFeatureProvider 
         Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
 
         // Return null if cursor is null or empty
-        if (cursor == null || !cursor.moveToFirst())
+        if (cursor == null || !cursor.moveToFirst()) {
+            try {
+                cursor.close();
+            }
+            catch (NullPointerException nullPointerException) {
+                 // cursor might be null
+            }
             return null;
+        }
 
         // Check if estimate is usage based
         int colIndex = cursor.getColumnIndex(BATTERY_ESTIMATE_BASED_ON_USAGE_COL);
@@ -183,8 +190,15 @@ public class PowerUsageFeatureProviderImpl implements PowerUsageFeatureProvider 
         Cursor cursor = context.getContentResolver().query(uri, null, null, null, null);
 
         // Return null if cursor is null or empty
-        if (cursor == null || !cursor.moveToFirst())
+        if (cursor == null || !cursor.moveToFirst()) {
+            try {
+                cursor.close();
+            }
+            catch (NullPointerException nullPointerException) {
+                 // cursor might be null
+            }
             return null;
+        }
 
         // Get time/battery data indicies
         int timestamp = cursor.getColumnIndex(TIMESTAMP_COL);
@@ -264,8 +278,15 @@ public class PowerUsageFeatureProviderImpl implements PowerUsageFeatureProvider 
         Cursor cursor = context.getContentResolver().query(builder.build(), null, null, null, null);
 
         // Return null if cursor is null or empty
-        if (cursor == null || !cursor.moveToFirst())
+        if (cursor == null || !cursor.moveToFirst()) {
+            try {
+                cursor.close();
+            }
+            catch (NullPointerException nullPointerException) {
+                 // cursor might be null
+            }
             return false;
+        }
 
         // Check if early warning is available
         boolean earlyWarningAvailable  = cursor.getInt(cursor.getColumnIndex(IS_EARLY_WARNING_COL)) == 1;
