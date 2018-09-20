8259f69c9510b729738022515e246f0267dac740
maxwen <max.weninger@gmail.com>
2018-09-20 00:55:16 -0500

    Configure System theme [2/2]
    Allow to force light or dark or use the wallpaper based
    automatic selection
    
    [Kshitij Gupta]
    
    - Add a preference controller so as to adapt code for Display Settings
    - Refactor some variables. We're theming System, not just SystemUI
    
    Change-Id: Ia2361b9db7009e2a3806b65b22c7e0532e6ebeed
    Signed-off-by: Josh Fox (XlxFoXxlX) <joshfox87@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -38,6 +38,7 @@ import com.android.settings.display.NightDisplayPreferenceController;
 import com.android.settings.display.NightModePreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
+import com.android.settings.display.SystemThemePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
@@ -139,6 +140,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
+        controllers.add(new SystemThemePreferenceController(context));
         controllers.add(new WallpaperPreferenceController(context));
         controllers.add(new ThemePreferenceController(context));
         controllers.add(new BrightnessLevelPreferenceController(context, lifecycle));
