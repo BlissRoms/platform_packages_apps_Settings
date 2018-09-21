9587b23f05829b57c4a7f2538ab38dacddf18bcd
davidevinavil <davidevinavil@gmail.com>
2018-09-21 02:11:35 +0200

    Home wake up: Make home button wake up opt-in [1/2]
    Change-Id: I8a770066a1090c4b32ab9f7af385854f91e568f1

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -27,6 +27,7 @@ import com.android.settings.display.BrightnessLevelPreferenceController;
 import com.android.settings.display.CameraGesturePreferenceController;
 import com.android.settings.display.LiftToWakePreferenceController;
 import com.android.settings.display.PocketJudgePreferenceController;
+import com.android.settings.display.HomeWakeButtonPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
@@ -92,6 +93,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new CameraGesturePreferenceController(context));
         controllers.add(new LiftToWakePreferenceController(context));
         controllers.add(new PocketJudgePreferenceController(context));
+        controllers.add(new HomeWakeButtonPreferenceController(context));
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
         controllers.add(new ForcePeakRefreshRatePreferenceController(context));
