16ccc27a813542a3477a71dcad68f8fe229c291e
DennySPB <dennyspb@gmail.com>
2019-09-15 04:17:29 +0200

    Battery: show battery temp on power summary
    Signed-off-by: mdeejay <mdjrussia@gmail.com>
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    Change-Id: Iff2ed19f59f9b9edcc466840a1ba0d385e29ff07
    Signed-off-by: rohan <purohit.rohan@gmail.com>

--- src/com/android/settings/fuelgauge/PowerUsageSummary.java
+++ src/com/android/settings/fuelgauge/PowerUsageSummary.java
@@ -82,6 +82,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     static final String ARG_BATTERY_LEVEL = "key_battery_level";
 
     private static final String KEY_SCREEN_USAGE = "screen_usage";
+    private static final String KEY_BATTERY_TEMP = "battery_temp";
     private static final String KEY_TIME_SINCE_LAST_FULL_CHARGE = "last_full_charge";
 
     @VisibleForTesting
@@ -96,6 +97,8 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
     int mBatteryLevel;
     @VisibleForTesting
     PowerGaugePreference mScreenUsagePref;
+    @VisibleForTesting
+    PowerGaugePreference mBatteryTempPref;
     @VisibleForTesting
     PowerGaugePreference mLastFullChargePref;
     @VisibleForTesting
@@ -232,6 +235,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
                 com.android.internal.R.integer.config_criticalBatteryWarningLevel) + 1;
 
         mScreenUsagePref = (PowerGaugePreference) findPreference(KEY_SCREEN_USAGE);
+        mBatteryTempPref = (PowerGaugePreference) findPreference(KEY_BATTERY_TEMP);
         mLastFullChargePref = (PowerGaugePreference) findPreference(
                 KEY_TIME_SINCE_LAST_FULL_CHARGE);
         mBatteryUtils = BatteryUtils.getInstance(getContext());
@@ -339,6 +343,7 @@ public class PowerUsageSummary extends PowerUsageBase implements OnLongClickList
         updateLastFullChargePreference();
         mScreenUsagePref.setSummary(StringUtil.formatElapsedTime(getContext(),
                 mBatteryUtils.calculateScreenUsageTime(mStatsHelper), false));
+        mBatteryTempPref.setSubtitle(BatteryInfo.batteryTemp+" "+Character.toString ((char) 176) + "C");
 
         final long elapsedRealtimeUs = SystemClock.elapsedRealtime() * 1000;
         Intent batteryBroadcast = context.registerReceiver(null,
