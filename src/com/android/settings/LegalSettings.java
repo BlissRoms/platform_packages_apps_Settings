LineageOS/android_packages_apps_Settings

Settings: Add LineageOS legal info
Open up the LineageOS legal info in the browser.

Change-Id: I263ccc0509e275d17512528deb606341d58e7a0d
Ticket-Id: CYNGNOS-1895
Signed-off-by: Roman Birg <roman@cyngn.com>

src/com/android/settings/LegalSettings.java
@@ -22,9 +22,14 @@ import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.SystemProperties;
 import android.provider.SearchIndexableResource;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceGroup;
+import android.support.v7.preference.PreferenceScreen;
+import android.util.Log;
 
 import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -36,11 +41,14 @@ import java.util.List;
 
 public class LegalSettings extends SettingsPreferenceFragment implements Indexable {
 
+    private static final String LOG_TAG = "LegalSettings";
     private static final String KEY_TERMS = "terms";
     private static final String KEY_LICENSE = "license";
     private static final String KEY_COPYRIGHT = "copyright";
     private static final String KEY_WEBVIEW_LICENSE = "webview_license";
     private static final String KEY_WALLPAPER_ATTRIBUTIONS = "wallpaper_attributions";
+    private static final String PROPERTY_LINEAGELICENSE_URL = "ro.lineagelegal.url";
+    private static final String KEY_LINEAGE_LICENSE = "lineagelicense";
 
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -57,6 +65,22 @@ public class LegalSettings extends SettingsPreferenceFragment implements Indexab
                 Utils.UPDATE_PREFERENCE_FLAG_SET_TITLE_TO_MATCHING_ACTIVITY);
         Utils.updatePreferenceToSpecificActivityOrRemove(act, parentPreference, KEY_WEBVIEW_LICENSE,
                 Utils.UPDATE_PREFERENCE_FLAG_SET_TITLE_TO_MATCHING_ACTIVITY);
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference.getKey().equals(KEY_LINEAGE_LICENSE)) {
+            String userLineageLicenseUrl = SystemProperties.get(PROPERTY_LINEAGELICENSE_URL);
+            final Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.setData(Uri.parse(userLineageLicenseUrl));
+            try {
+                startActivity(intent);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     @Override

BlissRoms/platform_packages_apps_Settings

src/com/android/settings/LegalSettings.java
@@ -18,7 +18,12 @@ package com.android.settings;
 
 import android.app.settings.SettingsEnums;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.SystemProperties;
 import android.provider.SearchIndexableResource;
+import android.util.Log;
+import androidx.preference.Preference;
 
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
@@ -33,6 +38,9 @@ public class LegalSettings extends DashboardFragment {
 
     private static final String TAG = "LegalSettings";
 
+    private static final String PROPERTY_LINEAGELICENSE_URL = "ro.lineagelegal.url";
+    private static final String KEY_LINEAGE_LICENSE = "lineagelicense";
+
     @Override
     public int getMetricsCategory() {
         return SettingsEnums.ABOUT_LEGAL_SETTINGS;
@@ -41,6 +49,22 @@ public class LegalSettings extends DashboardFragment {
     @Override
     protected String getLogTag() {
         return TAG;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference.getKey().equals(KEY_LINEAGE_LICENSE)) {
+            String userLineageLicenseUrl = SystemProperties.get(PROPERTY_LINEAGELICENSE_URL);
+            final Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            intent.setData(Uri.parse(userLineageLicenseUrl));
+            try {
+                startActivity(intent);
+            } catch (Exception e) {
+                Log.e(TAG, "Unable to start activity " + intent.toString());
+            }
+        }
+        return super.onPreferenceTreeClick(preference);
     }
 
     @Override