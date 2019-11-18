Havoc-OS/android_packages_apps_Settings

Settings: Add gestures control [4/4]
* Key contributors
@grarak
@carlosavignano
* Place screen off gesture preferences inside existing system
  gestures activity.
* Add support for new "S" gesture

Ticket: NOUGAT-17

Change-Id: I86d643e0d6f952b7d81e76090052fdb379d05c18
Signed-off-by: Carlo Savignano <carlosavignano@aospa.co>
Signed-off-by: TheStrix <parthbhatia98@gmail.com>
[TheStrix: Forward-ported to Pie]

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import com.android.settings.GesturesSettings;
 
 import com.havoc.config.center.ConfigCenter;
 
@@ -288,7 +289,8 @@ public class SettingsGateway {
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
             LiveDisplaySettings.class.getName(),
-            ConfigCenter.class.getName()
+            ConfigCenter.class.getName(),
+            GesturesSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {

BlissRoms/platform_packages_apps_Settings

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -148,6 +148,7 @@ import com.android.settings.wifi.calling.WifiCallingSettings;
 import com.android.settings.wifi.p2p.WifiP2pSettings;
 import com.android.settings.wifi.savedaccesspoints.SavedAccessPointsWifiSettings;
 import com.android.settings.wifi.tether.WifiTetherSettings;
+import com.android.settings.GesturesSettings;
 
 import com.blissroms.blissify.Blissify;
  
@@ -288,7 +289,8 @@ public class SettingsGateway {
             BatterySaverScheduleSettings.class.getName(),
             MobileNetworkListFragment.class.getName(),
             GlobalActionsPanelSettings.class.getName(),
-            Blissify.class.getName()
+            Blissify.class.getName(),
+            GesturesSettings.class.getName()
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {