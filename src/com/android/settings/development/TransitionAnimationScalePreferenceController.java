6b422e0e3035f9157e821a03dc1a9edfd051ab88
mydongistiny <jaysonedson@gmail.com>
2019-12-01 10:45:57 -0700

    AnimationSeekbar: Change default max and digits
    -Default: 0.60
    -Max: 1.75
    -Digits: X.XX (1.75)
    
    Change-Id: I92f33018158f0dbdac6aac4e6ce0ce0985d5e704

--- src/com/android/settings/development/TransitionAnimationScalePreferenceController.java
+++ src/com/android/settings/development/TransitionAnimationScalePreferenceController.java
@@ -38,7 +38,7 @@ public class TransitionAnimationScalePreferenceController extends
     @VisibleForTesting
     static final int TRANSITION_ANIMATION_SCALE_SELECTOR = 1;
     @VisibleForTesting
-    static final float DEFAULT_VALUE = 1.0f;
+    static final float DEFAULT_VALUE = 0.60f;
 
     private final IWindowManager mWindowManager;
     private final String[] mListValues;
