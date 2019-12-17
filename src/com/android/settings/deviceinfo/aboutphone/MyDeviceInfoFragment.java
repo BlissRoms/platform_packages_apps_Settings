93dd880bc15d3f5acbb43940197e5eaec936ff96
Alex Cruz <du.alexcruz@gmail.com>
2019-12-17 16:25:24 -0500

    Remove the "About phone" avatar
    Change-Id: I1004f27885731e23ecb5b478c4c0ba79914971b5
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
+++ src/com/android/settings/deviceinfo/aboutphone/MyDeviceInfoFragment.java
@@ -82,7 +82,7 @@ public class MyDeviceInfoFragment extends DashboardFragment
     @Override
     public void onStart() {
         super.onStart();
-        initHeader();
+        //initHeader();
     }
 
     @Override
