cd2622b3daf6e4e9c41c697ad66d1b75757a67bd
Alex <du.alexcruz@gmail.com>
2020-01-04 17:25:28 -0500

    Settings: Add avatar to search bar
    Alright, so technically the avatar code is already added to Settings but is missing
    some parts that Google has kept inside Pixel Impl's. We don't want to introduce any
    more reverse engineered code than we have to. Also, that heavily depends on the user
    flashing GAPPS. This method works with or without gapps, every time :p
    
    This commit kangs Google's method for showing the avatar as seen within the EmergencyInfo
    repo.
    
    https://github.com/DirtyUnicorns/android_packages_apps_EmergencyInfo/blob/q10x/src/com/android/emergency/preferences/EmergencyNamePreference.java#L213
    
    - Refresh avatar imageview onResume
    - Add click listener to link to User Settings for more options
    
    Change-Id: Id87843ebce8c33e9670ce10cff38c614ed2c25e0
    
    str: Rewritten most code for tm-release
    
    Co-authored-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Change-Id: Ic646d9d98b45a38569fa9fd084d033553ea5ddbf
    Signed-off-by: str143 <alexfinhart@gmail.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -26,12 +26,15 @@ import android.animation.LayoutTransition;
 import android.app.ActivityManager;
 import android.app.settings.SettingsEnums;
 import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.ApplicationInfoFlags;
 import android.content.pm.UserInfo;
 import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Process;
@@ -63,11 +66,11 @@ import androidx.window.embedding.SplitInfo;
 import androidx.window.embedding.SplitRule;
 import androidx.window.java.embedding.SplitControllerCallbackAdapter;
 
+import com.android.internal.util.UserIcons;
 import com.android.settings.R;
 import com.android.settings.Settings;
 import com.android.settings.SettingsActivity;
 import com.android.settings.SettingsApplication;
-import com.android.settings.accounts.AvatarViewMixin;
 import com.android.settings.activityembedding.ActivityEmbeddingRulesController;
 import com.android.settings.activityembedding.ActivityEmbeddingUtils;
 import com.android.settings.core.CategoryMixin;
@@ -77,6 +80,7 @@ import com.android.settings.overlay.FeatureFactory;
 import com.android.settings.safetycenter.SafetyCenterManagerWrapper;
 import com.android.settingslib.Utils;
 import com.android.settingslib.core.lifecycle.HideNonSystemOverlayMixin;
+import com.android.settingslib.drawable.CircleFramedDrawable;
 
 import com.google.android.setupcompat.util.WizardManagerHelper;
 
@@ -183,6 +187,10 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         return mCategoryMixin;
     }
 
+    Context context;
+    ImageView avatarView;
+    UserManager mUserManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -220,11 +228,42 @@ public class SettingsHomepageActivity extends FragmentActivity implements
 
         updateAppBarMinHeight();
         initHomepageContainer();
+
+        Context context = getApplicationContext();
+        mUserManager = context.getSystemService(UserManager.class);
+
         updateHomepageAppBar();
         updateHomepageBackground();
         mLoadedListeners = new ArraySet<>();
 
+        avatarView = findViewById(R.id.account_avatar);
+
+        if (avatarView != null) {
+          avatarView.setImageDrawable(getCircularUserIcon(getApplicationContext()));
+          avatarView.setVisibility(View.VISIBLE);
+          avatarView.setOnClickListener(new View.OnClickListener() {
+              @Override
+              public void onClick(View v) {
+                  Intent intent = new Intent(Intent.ACTION_MAIN);
+                  intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$UserSettingsActivity"));
+                  startActivity(intent);
+              }
+          });
+        }
+
         initSearchBarView();
+
+        avatarView = findViewById(R.id.account_avatar);
+        avatarView.setImageDrawable(getCircularUserIcon(context));
+        avatarView.setVisibility(View.VISIBLE);
+        avatarView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setComponent(new ComponentName("com.android.settings","com.android.settings.Settings$UserSettingsActivity"));
+                startActivity(intent);
+            }
+        });
 
         getLifecycle().addObserver(new HideNonSystemOverlayMixin(this));
         mCategoryMixin = new CategoryMixin(this);
@@ -233,7 +272,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
         final String highlightMenuKey = getHighlightMenuKey();
         // Only allow features on high ram devices.
         if (!getSystemService(ActivityManager.class).isLowRamDevice()) {
-            initAvatarView();
             final boolean scrollNeeded = mIsEmbeddingActivityEnabled
                     && !TextUtils.equals(getString(DEFAULT_HIGHLIGHT_MENU_KEY), highlightMenuKey);
             showSuggestionFragment(scrollNeeded);
@@ -381,20 +419,6 @@ public class SettingsHomepageActivity extends FragmentActivity implements
             FeatureFactory.getFeatureFactory().getSearchFeatureProvider()
                     .initSearchToolbar(this /* activity */, toolbarTwoPaneVersion,
                             SettingsEnums.SETTINGS_HOMEPAGE);
-        }
-    }
-
-    private void initAvatarView() {
-        final ImageView avatarView = findViewById(R.id.account_avatar);
-        final ImageView avatarTwoPaneView = findViewById(R.id.account_avatar_two_pane_version);
-        if (AvatarViewMixin.isAvatarSupported(this)) {
-            avatarView.setVisibility(View.VISIBLE);
-            getLifecycle().addObserver(new AvatarViewMixin(this, avatarView));
-
-            if (mIsEmbeddingActivityEnabled) {
-                avatarTwoPaneView.setVisibility(View.VISIBLE);
-                getLifecycle().addObserver(new AvatarViewMixin(this, avatarTwoPaneView));
-            }
         }
     }
 
@@ -795,6 +819,30 @@ public class SettingsHomepageActivity extends FragmentActivity implements
                 mIsSplitUpdatedUI = true;
                 mActivity.updateHomepageUI();
             }
+        }
+    }
+
+    private Drawable getCircularUserIcon(Context context) {
+    	final UserManager mUserManager = getSystemService(UserManager.class);
+        Bitmap bitmapUserIcon = mUserManager.getUserIcon(UserHandle.myUserId());
+
+        if (bitmapUserIcon == null) {
+            // get default user icon.
+            final Drawable defaultUserIcon = UserIcons.getDefaultUserIcon(
+                    context.getResources(), UserHandle.myUserId(), false);
+            bitmapUserIcon = UserIcons.convertToBitmap(defaultUserIcon);
+        }
+        Drawable drawableUserIcon = new CircleFramedDrawable(bitmapUserIcon,
+                (int) context.getResources().getDimension(com.android.internal.R.dimen.user_icon_size));
+
+        return drawableUserIcon;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (avatarView != null) {
+          avatarView.setImageDrawable(getCircularUserIcon(getApplicationContext()));
         }
     }
 }
