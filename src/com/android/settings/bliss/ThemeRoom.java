52bcfac30477425339800e35be86d6a6978dd104
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-18 22:05:39 -0700

    Introduce RGB Gradient Picker [2/2]
    Change-Id: I9501ba36188c34588af866957a2b0bcddc4ed3cc

--- src/com/android/settings/bliss/ThemeRoom.java
+++ src/com/android/settings/bliss/ThemeRoom.java
@@ -32,14 +32,20 @@ import java.util.HashSet;
 import com.android.settings.SettingsPreferenceFragment;
 import com.bliss.support.colorpicker.ColorPickerPreference;
 
-public class XThemeRoom extends SettingsPreferenceFragment implements
+public class ThemeRoom extends SettingsPreferenceFragment implements
         OnPreferenceChangeListener {
 
     private static final String ACCENT_COLOR = "accent_color";
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
+    private static final String GRADIENT_COLOR = "gradient_color";
+    private static final String GRADIENT_COLOR_PROP = "persist.sys.theme.gradientcolor";
 
     private IOverlayManager mOverlayService;
     private ColorPickerPreference mThemeColor;
+    private ColorPickerPreference mGradientColor;
+
+    static final int ACCENT = 0xFF1A73E8;
+    static final int GRADIENT = 0xFF1AD8E8;
 
     @Override
     public void onCreate(Bundle icicle) {
@@ -49,14 +55,25 @@ public class XThemeRoom extends SettingsPreferenceFragment implements
         mOverlayService = IOverlayManager.Stub
                 .asInterface(ServiceManager.getService(Context.OVERLAY_SERVICE));
         setupAccentPref();
+        setupGradientPref();
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object objValue) {
         if (preference == mThemeColor) {
             int color = (Integer) objValue;
-            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+            String hexColor = String.format("%08X", (0xFF1A73E8 & color));
             SystemProperties.set(ACCENT_COLOR_PROP, hexColor);
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+             } catch (RemoteException ignored) {
+             }
+        } else if (preference == mGradientColor) {
+            int color = (Integer) objValue;
+            String hexColor = String.format("%08X", (0xFF1AD8E8 & color));
+            SystemProperties.set(GRADIENT_COLOR_PROP, hexColor);
             try {
                  mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
                  mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
@@ -71,10 +88,20 @@ public class XThemeRoom extends SettingsPreferenceFragment implements
         mThemeColor = (ColorPickerPreference) findPreference(ACCENT_COLOR);
         String colorVal = SystemProperties.get(ACCENT_COLOR_PROP, "-1");
         int color = "-1".equals(colorVal)
-                ? Color.WHITE
+                ? ACCENT
                 : Color.parseColor("#" + colorVal);
         mThemeColor.setNewPreviewColor(color);
         mThemeColor.setOnPreferenceChangeListener(this);
+    }
+
+    private void setupGradientPref() {
+        mGradientColor = (ColorPickerPreference) findPreference(GRADIENT_COLOR);
+        String colorVal = SystemProperties.get(GRADIENT_COLOR_PROP, "-1");
+        int color = "-1".equals(colorVal)
+                ? GRADIENT
+                : Color.parseColor("#" + colorVal);
+        mGradientColor.setNewPreviewColor(color);
+        mGradientColor.setOnPreferenceChangeListener(this);
     }
 
     @Override
