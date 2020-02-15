b860d86f0a7932badf6b98633cbfa5c06f543f24
kubersharma001 <kubersharma001@gmail.com>
2020-02-15 15:03:42 +0100

    Settings: Hide|disable "Phone number" from About phone information
     This is more of a duplicate since AOSP
     already shows phone number inside "Sim Status"
     Showing phone number info here does not
     actually make sense since this can be seen
     via multiple ways from "Sim Status" info or
     via *#*#4636#*#* among other ways so let's
     hide it.
     Also, in many cases, sim doesn't have
     the number stored hence it shows:
     "Unknown number" which is just wasted.
    
    test:
    Open Settings: About phone, the "Phone number"
    for both sims, are not visible anymore, Also
    verify that phone number is still shown inside
    "Sim Status" information.
    
    Change-Id: I219fcf47493e913970f6cd5b4fb3a252390d0295
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/PhoneNumberPreferenceController.java
@@ -59,7 +59,7 @@ public class PhoneNumberPreferenceController extends BasePreferenceController {
         if (!mContext.getSystemService(UserManager.class).isAdminUser()) {
             return DISABLED_FOR_USER;
         }
-        return AVAILABLE;
+        return UNSUPPORTED_ON_DEVICE;
     }
 
     @Override
