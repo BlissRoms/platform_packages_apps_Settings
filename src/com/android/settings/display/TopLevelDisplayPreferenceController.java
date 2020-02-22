44d2414aaeeca8ef879be4563bcdd12c5a900d22
rohan <purohit.rohan@gmail.com>
2020-02-22 09:46:35 -0500

    Revert "Show Styles & Wallpaper in Settings"
    This reverts commit 2a9911e3a0efe3e1c39f3a98144fd39c36c4dedc.
    
    Change-Id: I7f877617803f8af13e950fc46714659131fa34c4

--- src/com/android/settings/display/TopLevelDisplayPreferenceController.java
+++ src/com/android/settings/display/TopLevelDisplayPreferenceController.java
@@ -36,15 +36,11 @@ public class TopLevelDisplayPreferenceController extends BasePreferenceControlle
 
     @Override
     public CharSequence getSummary() {
-        final WallpaperPreferenceController controller =
-                new WallpaperPreferenceController(mContext, "dummy_key");
-        if (controller.isAvailable()) {
-            return mContext.getText(
-                    controller.areStylesAvailable()
-                    ? R.string.display_dashboard_summary_with_style
-                    : R.string.display_dashboard_summary);
+        if (new WallpaperPreferenceController(mContext, "dummy_key").isAvailable()) {
+            return mContext.getText(R.string.display_dashboard_summary);
         } else {
             return mContext.getText(R.string.display_dashboard_nowallpaper_summary);
         }
     }
+
 }
