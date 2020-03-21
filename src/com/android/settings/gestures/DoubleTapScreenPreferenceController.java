AICP/packages_apps_Settings

Settings: doze: custom packages have priority
If the device has custom doze packages, such as CustomDoze
or OnePlusDoze, or simply Doze, then the management of what
is available and toggled on will be handled in there. So no
need to have duplicates under the display category

Change-Id: I05a83aade788b85e8caae54da0dd0b65df255daf

src/com/android/settings/gestures/DoubleTapScreenPreferenceController.java
@@ -28,6 +28,8 @@ import android.text.TextUtils;
 
 import androidx.annotation.VisibleForTesting;
 
+import com.android.internal.util.aicp.PackageUtils;
+
 public class DoubleTapScreenPreferenceController extends GesturePreferenceController {
 
     private final int ON = 1;
@@ -40,10 +42,12 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
     private final int mUserId;
+    private Context mContext;
 
     public DoubleTapScreenPreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
+        mContext = context;
     }
 
     public DoubleTapScreenPreferenceController setConfig(AmbientDisplayConfiguration config) {
@@ -64,8 +68,10 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
 
     @Override
     public int getAvailabilityStatus() {
-        // No hardware support for Double Tap
-        if (!getAmbientConfig().doubleTapSensorAvailable()) {
+        // No hardware support for Double Tap or
+        // custom touch gestures package is available
+        if (!getAmbientConfig().doubleTapSensorAvailable() ||
+                PackageUtils.isTouchGesturesPackageAvailable(mContext)) {
             return UNSUPPORTED_ON_DEVICE;
         }
 

src/com/android/settings/gestures/PickupGesturePreferenceController.java
@@ -26,6 +26,8 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.aicp.PackageUtils;
+
 public class PickupGesturePreferenceController extends GesturePreferenceController {
 
     private static final int ON = 1;
@@ -39,10 +41,12 @@ public class PickupGesturePreferenceController extends GesturePreferenceControll
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
     private final int mUserId;
+    private Context mContext;
 
     public PickupGesturePreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
+        mContext = context;
         mPickUpPrefKey = key;
     }
 
@@ -59,8 +63,10 @@ public class PickupGesturePreferenceController extends GesturePreferenceControll
 
     @Override
     public int getAvailabilityStatus() {
-        // No hardware support for Pickup Gesture
-        if (!getAmbientConfig().dozePickupSensorAvailable()) {
+        // No hardware support for Pickup Gesture or
+        // custom touch gestures package is available
+        if (!getAmbientConfig().dozePickupSensorAvailable() ||
+                PackageUtils.isTouchGesturesPackageAvailable(mContext)) {
             return UNSUPPORTED_ON_DEVICE;
         }
 

src/com/android/settings/gestures/TapScreenGesturePreferenceController.java
@@ -25,6 +25,8 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.aicp.PackageUtils;
+
 public class TapScreenGesturePreferenceController extends GesturePreferenceController {
 
     private static final String PREF_KEY_VIDEO = "gesture_tap_screen_video";
@@ -33,9 +35,12 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
     @UserIdInt
     private final int mUserId;
 
+    private Context mContext;
+
     public TapScreenGesturePreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
+        mContext = context;
     }
 
     public TapScreenGesturePreferenceController setConfig(AmbientDisplayConfiguration config) {
@@ -45,8 +50,10 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
 
     @Override
     public int getAvailabilityStatus() {
-        // No hardware support for this Gesture
-        if (!getAmbientConfig().tapSensorAvailable()) {
+        // No hardware support for this Gesture or
+        // custom touch gestures package is available
+        if (!getAmbientConfig().tapSensorAvailable() ||
+                PackageUtils.isTouchGesturesPackageAvailable(mContext)) {
             return UNSUPPORTED_ON_DEVICE;
         }
 

BlissRoms/platform_packages_apps_Settings

src/com/android/settings/gestures/DoubleTapScreenPreferenceController.java
@@ -28,6 +28,8 @@ import android.text.TextUtils;
 
 import androidx.annotation.VisibleForTesting;
 
+import com.android.internal.util.bliss.BlissUtils;
+
 public class DoubleTapScreenPreferenceController extends GesturePreferenceController {
 
     private final int ON = 1;
@@ -40,10 +42,12 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
     private final int mUserId;
+    private Context mContext;
 
     public DoubleTapScreenPreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
+        mContext = context;
     }
 
     public DoubleTapScreenPreferenceController setConfig(AmbientDisplayConfiguration config) {
@@ -64,8 +68,10 @@ public class DoubleTapScreenPreferenceController extends GesturePreferenceContro
 
     @Override
     public int getAvailabilityStatus() {
-        // No hardware support for Double Tap
-        if (!getAmbientConfig().doubleTapSensorAvailable()) {
+        // No hardware support for Double Tap or
+        // custom touch gestures package is available
+        if (!getAmbientConfig().doubleTapSensorAvailable() ||
+                BlissUtils.isTouchGesturesPackageAvailable(mContext)) {
             return UNSUPPORTED_ON_DEVICE;
         }
 

src/com/android/settings/gestures/PickupGesturePreferenceController.java
@@ -26,6 +26,8 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.bliss.BlissUtils;
+
 public class PickupGesturePreferenceController extends GesturePreferenceController {
 
     private static final int ON = 1;
@@ -39,10 +41,12 @@ public class PickupGesturePreferenceController extends GesturePreferenceControll
     private AmbientDisplayConfiguration mAmbientConfig;
     @UserIdInt
     private final int mUserId;
+    private Context mContext;
 
     public PickupGesturePreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
+        mContext = context;
         mPickUpPrefKey = key;
     }
 
@@ -59,8 +63,10 @@ public class PickupGesturePreferenceController extends GesturePreferenceControll
 
     @Override
     public int getAvailabilityStatus() {
-        // No hardware support for Pickup Gesture
-        if (!getAmbientConfig().dozePickupSensorAvailable()) {
+        // No hardware support for Pickup Gesture or
+        // custom touch gestures package is available
+        if (!getAmbientConfig().dozePickupSensorAvailable() ||
+                BlissUtils.isTouchGesturesPackageAvailable(mContext)) {
             return UNSUPPORTED_ON_DEVICE;
         }
 

src/com/android/settings/gestures/TapScreenGesturePreferenceController.java
@@ -25,6 +25,8 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.bliss.BlissUtils;
+
 public class TapScreenGesturePreferenceController extends GesturePreferenceController {
 
     private static final String PREF_KEY_VIDEO = "gesture_tap_screen_video";
@@ -33,9 +35,12 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
     @UserIdInt
     private final int mUserId;
 
+    private Context mContext;
+
     public TapScreenGesturePreferenceController(Context context, String key) {
         super(context, key);
         mUserId = UserHandle.myUserId();
+        mContext = context;
     }
 
     public TapScreenGesturePreferenceController setConfig(AmbientDisplayConfiguration config) {
@@ -45,8 +50,10 @@ public class TapScreenGesturePreferenceController extends GesturePreferenceContr
 
     @Override
     public int getAvailabilityStatus() {
-        // No hardware support for this Gesture
-        if (!getAmbientConfig().tapSensorAvailable()) {
+        // No hardware support for this Gesture or
+        // custom touch gestures package is available
+        if (!getAmbientConfig().tapSensorAvailable() ||
+                BlissUtils.isTouchGesturesPackageAvailable(mContext)) {
             return UNSUPPORTED_ON_DEVICE;
         }
 