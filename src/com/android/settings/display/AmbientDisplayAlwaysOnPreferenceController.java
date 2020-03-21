AICP/packages_apps_Settings

Settings: doze: custom packages have priority
If the device has custom doze packages, such as CustomDoze
or OnePlusDoze, or simply Doze, then the management of what
is available and toggled on will be handled in there. So no
need to have duplicates under the display category

Change-Id: I05a83aade788b85e8caae54da0dd0b65df255daf

src/com/android/settings/display/AmbientDisplayAlwaysOnPreferenceController.java
@@ -22,6 +22,7 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.aicp.PackageUtils;
 import com.android.settings.core.TogglePreferenceController;
 
 public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreferenceController {
@@ -34,6 +35,7 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
 
     private AmbientDisplayConfiguration mConfig;
     private OnPreferenceChangedCallback mCallback;
+    private Context mContext;
 
     public interface OnPreferenceChangedCallback {
         void onPreferenceChanged();
@@ -41,12 +43,14 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
 
     public AmbientDisplayAlwaysOnPreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
     }
 
     @Override
     public int getAvailabilityStatus() {
         return isAvailable(getConfig())
-                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false) ?
+                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false)
+                && !PackageUtils.isDozePackageAvailable(mContext) ?
                 AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -25,6 +25,8 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
+import com.android.internal.util.aicp.PackageUtils;
+
 import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
@@ -41,9 +43,11 @@ public class AmbientDisplayNotificationsPreferenceController extends
 
     private final MetricsFeatureProvider mMetricsFeatureProvider;
     private AmbientDisplayConfiguration mConfig;
+    private Context mContext;
 
     public AmbientDisplayNotificationsPreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
     }
 
@@ -80,6 +84,7 @@ public class AmbientDisplayNotificationsPreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return getAmbientConfig().pulseOnNotificationAvailable()
+                && !PackageUtils.isDozePackageAvailable(mContext)
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/PulseOnNewTracksPreferenceController.java
@@ -25,6 +25,8 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
+import com.android.internal.util.aicp.PackageUtils;
+
 import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
@@ -41,9 +43,11 @@ public class PulseOnNewTracksPreferenceController extends
 
     private final MetricsFeatureProvider mMetricsFeatureProvider;
     private AmbientDisplayConfiguration mConfig;
+    private Context mContext;
 
     public PulseOnNewTracksPreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
     }
 
@@ -82,6 +86,7 @@ public class PulseOnNewTracksPreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return getAmbientConfig().pulseOnNotificationAvailable()
+                && !PackageUtils.isDozePackageAvailable(mContext)
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

BlissRoms/platform_packages_apps_Settings

src/com/android/settings/display/AmbientDisplayAlwaysOnPreferenceController.java
@@ -22,6 +22,7 @@ import android.os.UserHandle;
 import android.provider.Settings;
 import android.text.TextUtils;
 
+import com.android.internal.util.bliss.BlissUtils;
 import com.android.settings.core.TogglePreferenceController;
 
 public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreferenceController {
@@ -34,6 +35,7 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
 
     private AmbientDisplayConfiguration mConfig;
     private OnPreferenceChangedCallback mCallback;
+    private Context mContext;
 
     public interface OnPreferenceChangedCallback {
         void onPreferenceChanged();
@@ -41,12 +43,14 @@ public class AmbientDisplayAlwaysOnPreferenceController extends TogglePreference
 
     public AmbientDisplayAlwaysOnPreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
     }
 
     @Override
     public int getAvailabilityStatus() {
         return isAvailable(getConfig())
-                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false) ?
+                && !SystemProperties.getBoolean(PROP_AWARE_AVAILABLE, false)
+                && !BlissUtils.isDozePackageAvailable(mContext) ?
                 AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/AmbientDisplayNotificationsPreferenceController.java
@@ -25,6 +25,8 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
+import com.android.internal.util.bliss.BlissUtils;
+
 import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
@@ -41,9 +43,11 @@ public class AmbientDisplayNotificationsPreferenceController extends
 
     private final MetricsFeatureProvider mMetricsFeatureProvider;
     private AmbientDisplayConfiguration mConfig;
+    private Context mContext;
 
     public AmbientDisplayNotificationsPreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
     }
 
@@ -80,6 +84,7 @@ public class AmbientDisplayNotificationsPreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return getAmbientConfig().pulseOnNotificationAvailable()
+                && !BlissUtils.isDozePackageAvailable(mContext)
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 

src/com/android/settings/display/PulseOnNewTracksPreferenceController.java
@@ -25,6 +25,8 @@ import android.text.TextUtils;
 import androidx.annotation.VisibleForTesting;
 import androidx.preference.Preference;
 
+import com.android.internal.util.bliss.BlissUtils;
+
 import com.android.settings.core.TogglePreferenceController;
 import com.android.settings.overlay.FeatureFactory;
 import com.android.settingslib.core.instrumentation.MetricsFeatureProvider;
@@ -41,9 +43,11 @@ public class PulseOnNewTracksPreferenceController extends
 
     private final MetricsFeatureProvider mMetricsFeatureProvider;
     private AmbientDisplayConfiguration mConfig;
+    private Context mContext;
 
     public PulseOnNewTracksPreferenceController(Context context, String key) {
         super(context, key);
+        mContext = context;
         mMetricsFeatureProvider = FeatureFactory.getFactory(context).getMetricsFeatureProvider();
     }
 
@@ -82,6 +86,7 @@ public class PulseOnNewTracksPreferenceController extends
     @Override
     public int getAvailabilityStatus() {
         return getAmbientConfig().pulseOnNotificationAvailable()
+                && !BlissUtils.isDozePackageAvailable(mContext)
                 ? AVAILABLE : UNSUPPORTED_ON_DEVICE;
     }
 