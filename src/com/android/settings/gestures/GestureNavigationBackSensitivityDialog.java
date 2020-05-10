065ef75126b1fd09e05d650be06165471502fb2d
ezio84 <brabus84@gmail.com>
2020-05-10 22:17:20 +0300

    Allow to hide navigation bar gesture completely [2/2]
    Change-Id: I439e8c79c42cff94aeb62feb1d03611caef29f5e

--- src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
+++ src/com/android/settings/gestures/GestureNavigationBackSensitivityDialog.java
@@ -39,15 +39,18 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
     private boolean mArrowSwitchChecked;
 
     private boolean mGestureHapticChecked;
+    private boolean mBlockImeChecked;
+    private boolean mShowNavChecked;
 
     private static final String TAG = "GestureNavigationBackSensitivityDialog";
     private static final String KEY_BACK_SENSITIVITY = "back_sensitivity";
     private static final String KEY_BACK_HEIGHT = "back_height";
     private static final String KEY_HOME_HANDLE_SIZE = "home_handle_width";
     private static final String KEY_BACK_BLOCK_IME = "back_block_ime";
+    private static final String KEY_SHOW_NAV = "show_nav";
 
     public static void show(SystemNavigationGestureSettings parent, int sensitivity, int height,
-            int length, boolean blockIme) {
+            boolean blockIme, int length, boolean showNav) {
         if (!parent.isAdded()) {
             return;
         }
@@ -59,6 +62,7 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
         bundle.putInt(KEY_BACK_HEIGHT, height);
         bundle.putInt(KEY_HOME_HANDLE_SIZE, length);
         bundle.putBoolean(KEY_BACK_BLOCK_IME, blockIme);
+        bundle.putBoolean(KEY_SHOW_NAV, showNav);
         dialog.setArguments(bundle);
         dialog.setTargetFragment(parent, 0);
         dialog.show(parent.getFragmentManager(), TAG);
@@ -101,6 +105,17 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                 mGestureHapticChecked = GestureHapticSwitch.isChecked() ? true : false;
             }
         });
+        final Switch showNavSwitch = view.findViewById(R.id.show_gestures_navbar);
+        mShowNavChecked = Settings.System.getInt(getActivity().getContentResolver(),
+                Settings.System.GESTURE_NAVBAR_SHOW, 1) == 1;
+        showNavSwitch.setChecked(mShowNavChecked);
+        showNavSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mShowNavChecked = showNavSwitch.isChecked() ? true : false;
+            }
+        });
+
         return new AlertDialog.Builder(getContext())
                 .setTitle(R.string.back_options_dialog_title)
                 .setMessage(R.string.back_sensitivity_dialog_message)
@@ -114,6 +129,10 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                     getArguments().putInt(KEY_HOME_HANDLE_SIZE, length);
                     boolean blockIme = blockImeSwitch.isChecked();
                     getArguments().putBoolean(KEY_BACK_BLOCK_IME, blockIme);
+                    boolean showNav = showNavSwitch.isChecked();
+                    getArguments().putBoolean(KEY_SHOW_NAV, showNav);
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.GESTURE_NAVBAR_SHOW, mShowNavChecked ? 1 : 0);
                     SystemNavigationGestureSettings.setBackHeight(getActivity(), height);
                     SystemNavigationGestureSettings.setBackSensitivity(getActivity(),
                             getOverlayManager(), sensitivity);
@@ -123,6 +142,7 @@ public class GestureNavigationBackSensitivityDialog extends InstrumentedDialogFr
                             Settings.Secure.SHOW_BACK_ARROW_GESTURE, mArrowSwitchChecked ? 1 : 0);
                     Settings.System.putInt(getActivity().getContentResolver(),
                             Settings.System.BACK_GESTURE_HAPTIC, mGestureHapticChecked ? 1 : 0);
+                    SystemNavigationGestureSettings.setShowNav(getActivity(), showNav);
                 })
                 .create();
     }
