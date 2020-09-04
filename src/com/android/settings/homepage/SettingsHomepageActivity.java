bb17d0296f6eb9a0ee8c15c56ecd1c78b8d0a2e8
Rahul <rahul731999@gmail.com>
2020-09-04 08:10:10 +0000

    Settings: Revamp UI
    Change-Id: Idd4ba9cf1f6b9f2692d658d34e07ba26e2442890
    Co-authored-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>
    Co-authored-by: Rahul <rahul731999@gmail.com>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -30,8 +30,10 @@ import android.os.UserManager;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.provider.Settings;
 import android.widget.ImageView;
 import android.widget.Toolbar;
+import android.widget.TextView;
 import android.provider.Settings;
 import androidx.annotation.VisibleForTesting;
 import androidx.fragment.app.Fragment;
@@ -46,6 +48,8 @@ import com.android.settings.accounts.AvatarViewMixin;
 import com.android.settings.core.HideNonSystemOverlayMixin;
 import com.android.settings.homepage.contextualcards.ContextualCardsFragment;
 import com.android.settings.overlay.FeatureFactory;
+
+import java.util.Calendar;
 
 import com.android.settingslib.drawable.CircleFramedDrawable;
 
@@ -67,6 +71,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
                 View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
 
         setHomepageContainerPaddingTop();
+        goodVibesPlease();
 
         Context context = getApplicationContext();
 
@@ -76,8 +81,10 @@ public class SettingsHomepageActivity extends FragmentActivity {
         FeatureFactory.getFactory(this).getSearchFeatureProvider()
                 .initSearchToolbar(this /* activity */, toolbar, SettingsEnums.SETTINGS_HOMEPAGE);
 
+//        final ImageView avatarView = findViewById(R.id.account_avatar);
+//       getLifecycle().addObserver(new AvatarViewMixin(this, avatarView));
+        final TextView homepageUsernameTextView = root.findViewById(R.id.userNameTextView); 
         getLifecycle().addObserver(new HideNonSystemOverlayMixin(this));
-
         avatarView = root.findViewById(R.id.account_avatar);
         //final AvatarViewMixin avatarViewMixin = new AvatarViewMixin(this, avatarView);
         avatarView.setImageDrawable(getCircularUserIcon(context));
@@ -91,6 +98,8 @@ public class SettingsHomepageActivity extends FragmentActivity {
         });
         //getLifecycle().addObserver(avatarViewMixin);
 
+	String name = mUserManager.getUserName();
+	homepageUsernameTextView.setText(name!=null?name:"User");
 
         showFragment(new TopLevelSettings(), R.id.main_content);
         ((FrameLayout) findViewById(R.id.main_content))
@@ -120,7 +129,7 @@ public class SettingsHomepageActivity extends FragmentActivity {
 
     private boolean isHomepageSpacerEnabled() {
         return Settings.System.getInt(this.getContentResolver(),
-        Settings.System.SETTINGS_SPACER, 0) != 0;
+        Settings.System.SETTINGS_SPACER, 1) != 0;
     }
 
     private static void setMargins (View v, int l, int t, int r, int b) {
@@ -129,6 +138,37 @@ public class SettingsHomepageActivity extends FragmentActivity {
             p.setMargins(l, t, r, b);
             v.requestLayout();
         }
+    }
+
+     private void goodVibesPlease(){
+
+     Calendar c = Calendar.getInstance();
+     int hours = c.get(Calendar.HOUR_OF_DAY);
+     String greeting=null;
+     TextView homePageGreetingTextView=(TextView) findViewById(R.id.greetingsTextView);
+     View root = findViewById(R.id.settings_homepage_container);
+
+     if(hours>=0 && hours<=11){
+         greeting = "Good Morning, ";
+
+     } else if(hours>=12 && hours<=15){
+
+         greeting = "Good AfterNoon, ";
+
+     } else if(hours>=16 && hours<=20){
+
+         greeting = "Good Evening, ";
+
+     } else if(hours>=21 && hours<=24){
+
+         greeting = "Good Night, ";
+
+     } else {
+         greeting = "Welcome To Earth, ";
+     }
+
+     homePageGreetingTextView.setText(greeting);
+
     }
 
     @VisibleForTesting
@@ -166,6 +206,5 @@ public class SettingsHomepageActivity extends FragmentActivity {
     public void onResume() {
         super.onResume();
         avatarView.setImageDrawable(getCircularUserIcon(getApplicationContext()));
-
     }
 }
