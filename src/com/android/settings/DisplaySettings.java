dd3d9b862c1cecfa4e0146cf0a3ebacf9b244b62
Jyotiraditya <imjyotiraditya@pm.me>
2020-10-09 10:39:59 +0530

    Settings: Move blur toggle to Display options.
    Change-Id: I64cadfd69c66a30ada9f5ba5509dd97a85124326
    Signed-off-by: spkal01 <kalligeross@gmail.com>
    Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

--- src/com/android/settings/DisplaySettings.java
+++ src/com/android/settings/DisplaySettings.java
@@ -31,6 +31,7 @@ import com.android.settings.display.HomeWakeButtonPreferenceController;
 import com.android.settings.display.ScreenSaverPreferenceController;
 import com.android.settings.display.ShowOperatorNamePreferenceController;
 import com.android.settings.display.TapToWakePreferenceController;
+import com.android.settings.display.EnableBlursPreferenceController;
 import com.android.settings.display.ThemePreferenceController;
 import com.android.settings.display.TimeoutPreferenceController;
 import com.android.settings.display.VrDisplayPreferenceController;
@@ -89,6 +90,7 @@ public class DisplaySettings extends DashboardFragment {
         controllers.add(new HomeWakeButtonPreferenceController(context));      
         controllers.add(new ScreenSaverPreferenceController(context));
         controllers.add(new TapToWakePreferenceController(context));
+        controllers.add(new EnableBlursPreferenceController(context));
         controllers.add(new TimeoutPreferenceController(context, KEY_SCREEN_TIMEOUT));
         controllers.add(new VrDisplayPreferenceController(context));
         controllers.add(new ShowOperatorNamePreferenceController(context));
