2be58455bb4a13e7879b0ada8697670537a98237
Jackeagle <jackeagle102@gmail.com>
2021-05-24 09:03:15 -0400

    Settings: Move OverlayCategoryPreferenceController to Display Settings
    - previously was in Developer Settings
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Change-Id: I08fb1ec1b0fe563bfcb311b8de19671c4133997f

--- src/com/android/settings/development/EmulateDisplayCutoutPreferenceController.java
+++ src/com/android/settings/development/EmulateDisplayCutoutPreferenceController.java
@@ -21,6 +21,7 @@ import android.content.om.IOverlayManager;
 import android.content.pm.PackageManager;
 import android.os.ServiceManager;
 import android.view.DisplayCutout;
+import com.android.settings.display.OverlayCategoryPreferenceController;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -31,7 +32,7 @@ public class EmulateDisplayCutoutPreferenceController extends OverlayCategoryPre
     @VisibleForTesting
     EmulateDisplayCutoutPreferenceController(Context context, PackageManager packageManager,
             IOverlayManager overlayManager) {
-        super(context, packageManager, overlayManager, DisplayCutout.EMULATION_OVERLAY_CATEGORY);
+        super(context, DisplayCutout.EMULATION_OVERLAY_CATEGORY);
     }
 
     public EmulateDisplayCutoutPreferenceController(Context context) {
