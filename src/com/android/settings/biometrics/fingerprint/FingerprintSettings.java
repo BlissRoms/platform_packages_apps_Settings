PixelExperience/packages_apps_Settings

FingerprintSettings: Don't listen for fingerprint if fod present
* Bad UX

Change-Id: Ie7d698a19372d2afba4b38c212f85b3b27461d25

src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -66,6 +66,8 @@ import com.android.settingslib.widget.FooterPreference;
 import java.util.HashMap;
 import java.util.List;
 
+import com.android.internal.util.custom.FodUtils;
+
 /**
  * Settings screen for fingerprints
  */
@@ -256,6 +258,10 @@ public class FingerprintSettings extends SubSettings {
             // Don't start authentication if ChooseLockGeneric is showing, otherwise if the user
             // is in FP lockout, a toast will show on top
             if (mLaunchedConfirm) {
+                return;
+            }
+            // Don't listen if has fod support
+            if (FodUtils.hasFodSupport(getContext())){
                 return;
             }
             if (!mInFingerprintLockout) {

BlissRoms/platform_packages_apps_Settings

Signed-off-by: Ayan Mukherjee <mukherjeeayan725@gmail.com>

src/com/android/settings/biometrics/fingerprint/FingerprintSettings.java
@@ -66,6 +66,8 @@ import com.android.settingslib.widget.FooterPreference;
 import java.util.HashMap;
 import java.util.List;
 
+import com.android.internal.util.bliss.FodUtils;
+
 /**
  * Settings screen for fingerprints
  */
@@ -259,6 +261,10 @@ public class FingerprintSettings extends SubSettings {
             // Don't start authentication if ChooseLockGeneric is showing, otherwise if the user
             // is in FP lockout, a toast will show on top
             if (mLaunchedConfirm) {
+                return;
+            }
+            // Don't listen if has fod support
+            if (FodUtils.hasFodSupport(getContext())){
                 return;
             }
             if (!mInFingerprintLockout) {