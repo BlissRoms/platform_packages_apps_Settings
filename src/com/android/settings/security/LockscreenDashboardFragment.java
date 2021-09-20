250849787daf7d23fddb5822b497ffde02ad9ee2
Ido Ben-Hur <idoybh2@gmail.com>
2021-09-21 00:25:57 +0530

    Settings: Allow scheduling always on display [2/2]
    * Squash of
      https://github.com/yaap/packages_apps_YASP/commit/30441ec0a74e448274d8c7101fd3dc3aceade34f
      https://github.com/yaap/packages_apps_YASP/commit/b896aa6f5e7dbe29203bb81a46ff4ec8231a9aa8
      https://github.com/yaap/packages_apps_YASP/commit/f500a603254692e5a6c633b982ee181b4c633816
    
    [jhonboy121]: adaptd to A13 and use a preference controller to update the preference
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: I5f4b59e7e818b2190e8185554102e665c1410db6
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -32,11 +32,13 @@ import android.provider.Settings;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
+import androidx.preference.Preference;
 
 import com.android.settings.R;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
+import com.android.settings.display.AODSchedulePreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
 import com.android.settings.display.DozeOnChargePreferenceController;
@@ -56,7 +58,6 @@ import java.util.List;
 @SearchIndexable
 public class LockscreenDashboardFragment extends DashboardFragment
         implements OwnerInfoPreferenceController.OwnerInfoCallback {
-
     private static final String TAG = "LockscreenDashboardFragment";
 
     @VisibleForTesting
@@ -70,7 +71,6 @@ public class LockscreenDashboardFragment extends DashboardFragment
     @VisibleForTesting
     static final String KEY_ADD_USER_FROM_LOCK_SCREEN =
             "security_lockscreen_add_users_when_locked";
-
 
     private AmbientDisplayConfiguration mConfig;
     private OwnerInfoPreferenceController mOwnerInfoPreferenceController;
@@ -117,6 +117,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         use(DozeOnChargePreferenceController.class).setConfig(getConfig(context));
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
+        use(AODSchedulePreferenceController.class).setConfig(getConfig(context));
 
         mControlsContentObserver = new ContentObserver(
                 new Handler(Looper.getMainLooper())) {
