Evolution-X-AOSP/packages_apps_Settings

Settings: Expose Heads Up settings page
* Based on c80b3ab718af6bf0c1fa423007944a04da07e2d5.

Signed-off-by: Joey Huab <joey@evolution-x.org>

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -167,6 +167,7 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import com.evolution.settings.EvolutionSettings;
+import com.evolution.settings.fragments.HeadsUpSettings;
 
 public class SettingsGateway {
 
@@ -322,6 +323,7 @@ public class SettingsGateway {
             AlarmsAndRemindersDetails.class.getName(),
             MediaManagementAppsDetails.class.getName(),
             EvolutionSettings.class.getName(),
+            HeadsUpSettings.class.getName(),
             RunningServices.class.getName()
     };
 
@@ -363,6 +365,7 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.EvolutionSettingsActivity.class.getName(),
+            Settings.HeadsUpSettingsActivity.class.getName(),
             Settings.DevRunningServicesActivity.class.getName()
     };
 }

BlissRoms/platform_packages_apps_Settings

Change-Id: Ia6674897e69e5b4baa8db9a432d8a23d011c9ef7

src/com/android/settings/core/gateway/SettingsGateway.java
@@ -171,7 +171,8 @@ import com.android.settings.wifi.savedaccesspoints2.SavedAccessPointsWifiSetting
 import com.android.settings.wifi.tether.WifiTetherSettings;
 
 import org.blissroms.blissify.Blissify;
- 
+import org.blissroms.blissify.fragments.HeadsUpSettings;
+
 public class SettingsGateway {
 
     /**
@@ -330,7 +331,8 @@ public class SettingsGateway {
             NetworkSelectSettings.class.getName(),
             AlarmsAndRemindersDetails.class.getName(),
             MediaManagementAppsDetails.class.getName(),
-            Blissify.class.getName()
+            Blissify.class.getName(),
+            HeadsUpSettings.class.getName(),
     };
 
     public static final String[] SETTINGS_FOR_RESTRICTED = {
@@ -372,5 +374,6 @@ public class SettingsGateway {
             Settings.ModuleLicensesActivity.class.getName(),
             UserBackupSettingsActivity.class.getName(),
             Settings.BlissifySettingsActivity.class.getName(),
+            Settings.HeadsUpSettingsActivity.class.getName(),
     };
 }