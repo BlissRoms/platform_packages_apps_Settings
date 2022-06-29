a762f9cffd7f4afe935244b029511c406aa4c298
althafvly <althafvly@gmail.com>
2022-06-29 16:11:25 +0530

    Settings: Allow user configurable fingerprint wake-and-unlock
    Change-Id: I4ee28af18fcd387f51249358809bd79190a0cc8f
    Signed-off-by: aswin7469 <aswinas@pixysos.com>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/security/screenlock/ScreenLockSettings.java
+++ src/com/android/settings/security/screenlock/ScreenLockSettings.java
@@ -79,6 +79,7 @@ public class ScreenLockSettings extends DashboardFragment
         controllers.add(new LockAfterTimeoutPreferenceController(
                 context, MY_USER_ID, lockPatternUtils));
         controllers.add(new OwnerInfoPreferenceController(context, parent));
+        controllers.add(new FingerprintUnlockPreferenceController(context));
         return controllers;
     }
 
