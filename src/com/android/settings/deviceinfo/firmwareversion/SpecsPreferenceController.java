5735a498ea3cc7b860d00f4aad3f3aa68689244e
SuperDroidBond <superdroidbond@yahoo.com>
2022-09-15 20:19:00 +0530

    About Phone Specs: increase device RAM & Storage sizes
     SuperDroidBond:
     Now-a-days devices are having 256GB Space and 12 GB RAM.
     Let's make the phone specs tell actual specs of the phone.
     Also correct the RAM show correct value when at it.
    
    Signed-off-by: SuperDroidBond <superdroidbond@yahoo.com>

--- src/com/android/settings/deviceinfo/firmwareversion/SpecsPreferenceController.java
+++ src/com/android/settings/deviceinfo/firmwareversion/SpecsPreferenceController.java
@@ -38,6 +38,7 @@ public class SpecsPreferenceController extends BasePreferenceController {
     @VisibleForTesting
     static final String PROCESSOR_MODEL = "ro.processor.model";
     static String aprox;
+    static String aproxRam;
     public SpecsPreferenceController(Context context, String preferenceKey) {
         super(context, preferenceKey);
     }
@@ -55,34 +56,51 @@ public class SpecsPreferenceController extends BasePreferenceController {
         long totalBlocks = stat.getBlockCountLong();
         double total = (totalBlocks * blockSize)/ 1073741824;
         int lastval = (int) Math.round(total);
-            if ( lastval > 0  && lastval <= 16){
+            if (lastval > 0  && lastval <= 16){
                 aprox = "16";
-            } else if (lastval > 16 && lastval <=32) {
+            } else if (lastval > 16 && lastval <= 32) {
                 aprox = "32";
-            } else if (lastval > 32 && lastval <=64) {
+            } else if (lastval > 32 && lastval <= 64) {
                 aprox = "64";
-            } else if (lastval > 64 && lastval <=128) {
+            } else if (lastval > 64 && lastval <= 128) {
                 aprox = "128";
-            } else if (lastval > 128) {
-                aprox = "128+";
+            } else if (lastval > 128 && lastval <= 256) {
+                aprox = "256";
+            } else if (lastval > 256) {
+                aprox = "256+";
             } else aprox = "null";
         return aprox;
     }
 
     // Get total mem
-    public static int getTotalRAM() {
+    public static String getTotalRAM() {
             MemInfoReader memReader = new MemInfoReader();
             memReader.readMemInfo();
             String aprox;
             double totalmem = memReader.getTotalSize();
             double gb = (totalmem / 1073741824) + 0.1f; // Cause 4gig devices show memory as 3.48 .-.
             int gigs = (int) Math.round(gb);
-            return gigs;
+            if (gigs > 0  && gigs <= 2) {
+                aproxRam = "2";
+            } else if (gigs > 2 && gigs <= 3) {
+                aproxRam = "3";
+            } else if (gigs > 3 && gigs <= 4) {
+                aproxRam = "4";
+            } else if (gigs > 4 && gigs <= 6) {
+                aproxRam = "6";
+            } else if (gigs > 6 && gigs <= 8) {
+                aproxRam = "8";
+            } else if (gigs > 8 && gigs <= 12) {
+                aproxRam = "12";
+            } else if (gigs > 12) {
+                aproxRam = "12+";
+            } else aproxRam = "null";
+        return aproxRam;
     }
 
     @Override
     public CharSequence getSummary() {
-        String memory = Integer.toString(getTotalRAM()) + "GB | " + getTotalInternalMemorySize() + "GB";
+        String memory = getTotalRAM() + "GB | " + getTotalInternalMemorySize() + "GB";
         String cpu = SystemProperties.get(PROCESSOR_MODEL);
         if (!cpu.isEmpty()) {
             return cpu + " | " + memory;
