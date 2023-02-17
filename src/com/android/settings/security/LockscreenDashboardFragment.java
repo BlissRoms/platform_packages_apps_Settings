dc1262576c40fd13a39f70f28691af6cd254bd9a
darkobas <darkobas@gmail.com>
2023-02-17 14:44:34 +0800

    Settings: add option to enable AOD on charging only [2/2]
    [jhonboy121]:
        return availability status based on AmbientDisplayConfiguration just like AOD preference controller does
        disable doze on charge preference when doze always on is enabled
        rewrite in kt and observe settings change
    
    Co-authored-by: jhonboy121 <alfredmathew05@gmail.com>
    Change-Id: If5538ff0909c48e3bcb5863dee18dd14d4f4d331
    Signed-off-by: jhonboy121 <alfredmathew05@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/security/LockscreenDashboardFragment.java
+++ src/com/android/settings/security/LockscreenDashboardFragment.java
@@ -39,6 +39,7 @@ import com.android.settings.display.AmbientDisplayAlwaysOnPreferenceController;
 import com.android.settings.display.AmbientDisplayNotificationsPreferenceController;
 import com.android.settings.gestures.DoubleTapScreenPreferenceController;
 import com.android.settings.gestures.PickupGesturePreferenceController;
+import com.android.settings.display.DozeOnChargePreferenceController;
 import com.android.settings.notification.LockScreenNotificationPreferenceController;
 import com.android.settings.search.BaseSearchIndexProvider;
 import com.android.settings.security.screenlock.LockScreenPreferenceController;
@@ -113,6 +114,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
             use(AmbientDisplayAlwaysOnPreferenceController.class).setConfig(getConfig(context));
         }
         use(AmbientDisplayNotificationsPreferenceController.class).setConfig(getConfig(context));
+        use(DozeOnChargePreferenceController.class).setConfig(getConfig(context));
         use(DoubleTapScreenPreferenceController.class).setConfig(getConfig(context));
         use(PickupGesturePreferenceController.class).setConfig(getConfig(context));
 
@@ -151,6 +153,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
         controllers.add(notificationController);
         mOwnerInfoPreferenceController = new OwnerInfoPreferenceController(context, this);
         controllers.add(mOwnerInfoPreferenceController);
+        controllers.add(new DozeOnChargePreferenceController(context, lifecycle));
 
         return controllers;
     }
@@ -184,6 +187,7 @@ public class LockscreenDashboardFragment extends DashboardFragment
                     controllers.add(new LockScreenNotificationPreferenceController(context));
                     controllers.add(new OwnerInfoPreferenceController(
                             context, null /* fragment */));
+                    controllers.add(new DozeOnChargePreferenceController(context, null /* lifecycle */));
                     return controllers;
                 }
 
