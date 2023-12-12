4748d489b3f2cec4cc659b2eb9c5e369209949bc
Bruno Martins <bgcngm@gmail.com>
2023-12-12 16:12:32 +0000

    Settings: Move SIM restrictions currently applied to per-app data usage
    When Google introduced commit d3b35f2, all the custom addons for
    per-app network isolation became hidden on devices that set the
    overlay config_show_sim_info to "false".
    
    Expose only the toggles that really make sense, i.e. for the mentioned
    usecase, leave the main toggle (restrict all), WiFi and VPN data
    toggles.
    
    Change-Id: I4720483d98ae69fbb6eec8fa749e8d071e91c629
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/datausage/AppDataUsage.java
+++ src/com/android/settings/datausage/AppDataUsage.java
@@ -92,10 +92,6 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
     private long mSelectedCycle;
     private boolean mIsLoading;
 
-    public boolean isSimHardwareVisible(Context context) {
-        return SubscriptionUtil.isSimHardwareVisible(context);
-    }
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -138,7 +134,7 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
         final UidDetailProvider uidDetailProvider = getUidDetailProvider();
 
         if (mAppItem.key > 0) {
-            if ((!isSimHardwareVisible(mContext)) || !UserHandle.isApp(mAppItem.key)) {
+            if (!UserHandle.isApp(mAppItem.key)) {
                 final UidDetail uidDetail = uidDetailProvider.getUidDetail(mAppItem.key, true);
                 mIcon = uidDetail.icon;
                 mLabel = uidDetail.label;
@@ -276,9 +272,6 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
     }
 
     private void updatePrefs(boolean restrictBackground, boolean unrestrictData) {
-        if (!isSimHardwareVisible(mContext)) {
-            return;
-        }
         setBackPreferenceListAnimatorIfLoaded();
         final EnforcedAdmin admin = RestrictedLockUtilsInternal
                 .checkIfMeteredDataUsageUserControlDisabled(mContext, mPackageName,
