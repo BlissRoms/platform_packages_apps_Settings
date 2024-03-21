827828ff64568a8b40bf96122aac1bb8cda21af7
Tommy Webb <tommy@calyxinstitute.org>
2024-03-21 14:46:34 +0000

    DynamicDenylistManager shall not exist
    Remove DynamicDenylistManager to avoid its conflicting behavior
    with our firewall. This stops the resetting of firewall policies
    on boot and for other reasons. The user is in control of these
    policies and doesn't need them to be unexpectedly reset.
    
    Issue: calyxos#2211
    Test: Manual: Turn off Background network access for any app. Then,
    in Firewall, turn off another toggle like Wi-Fi. Reboot, and check
    the toggles. Background network access and Wi-Fi should still be off.
    Change-Id: Ie62ddfa3a893c9adf5d4fd0c8670235a5a51e03f
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/datausage/AppDataUsage.java
+++ src/com/android/settings/datausage/AppDataUsage.java
@@ -44,7 +44,6 @@ import com.android.settings.applications.AppInfoBase;
 import com.android.settings.datausage.lib.AppDataUsageDetailsRepository;
 import com.android.settings.datausage.lib.NetworkTemplates;
 import com.android.settings.datausage.lib.NetworkUsageDetailsData;
-import com.android.settings.fuelgauge.datasaver.DynamicDenylistManager;
 import com.android.settings.network.SubscriptionUtil;
 import com.android.settings.widget.EntityHeaderController;
 import com.android.settingslib.AppItem;
@@ -326,8 +325,7 @@ public class AppDataUsage extends DataUsageBaseFragment implements OnPreferenceC
     private boolean getAppRestrictBackground() {
         final int uid = mAppItem.key;
         final int uidPolicy = services.mPolicyManager.getUidPolicy(uid);
-        return (uidPolicy & POLICY_REJECT_METERED_BACKGROUND) != 0
-                && DynamicDenylistManager.getInstance(mContext).isInManualDenylist(uid);
+        return (uidPolicy & POLICY_REJECT_METERED_BACKGROUND) != 0;
     }
 
     private boolean getUnrestrictData() {
