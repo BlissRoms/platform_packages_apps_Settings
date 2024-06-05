e4b4e2207f36e8145cceb44c7ffe3feefcb8b670
Joey <joey@evolution-x.org>
2024-06-05 15:46:44 +0900

    Settings: Add a preference controller for Volume steps settings
    * Enable volume steps pref by default
    
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/sound/VolumeStepsSettings.java
+++ src/com/android/settings/sound/VolumeStepsSettings.java
@@ -37,10 +37,10 @@ import org.blissroms.blissify.preferences.CustomSeekBarPreference;
  * volume steps settings under sound
  */
 @SearchIndexable
-public class VolumeSteps extends SettingsPreferenceFragment implements
+public class VolumeStepsSettings extends SettingsPreferenceFragment implements
         Preference.OnPreferenceChangeListener {
 
-    private static final String TAG = "VolumeSteps";
+    private static final String TAG = "VolumeStepsSettings";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
