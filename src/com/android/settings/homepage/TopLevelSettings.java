ProjectRadiant/packages_apps_Settings

Settings: Better DeviceSettings IA handling
Check for a specific keyword rather than full key and allow people to overlay an IA string incase they use a different package name.
Not the cleanest way but it works

src/com/android/settings/homepage/TopLevelSettings.java
@@ -46,14 +46,15 @@ public class TopLevelSettings extends DashboardFragment implements
     private static final String TAG = "TopLevelSettings";
 
     public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
-        "dashboard_tile_pref_com.oneplus.extras.DeviceSettings",
-        "dashboard_tile_pref_org.lineageos.settings.device",
-        "dashboard_tile_pref_org.lineageos.settings",
-        "dashboard_tile_pref_org.omnirom.devices",
-        "dashboard_tile_pref_org.omnirom.device",
-        "dashboard_tile_pref_com.poco.parts",
-        "dashboard_tile_pref_com.xiaomi.parts",
-        "dashboard_tile_pref_com.asus.zenparts"
+        "oneplus",
+        "realme",
+        "lineage",
+        "omni",
+        "phh",
+        "poco",
+        "xiaomi",
+        "asus",
+        "moto"
     };
 
     public static final String[] CHANGE_LAYOUT_KEYS = {
@@ -125,19 +126,41 @@ public class TopLevelSettings extends DashboardFragment implements
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         super.onCreatePreferences(savedInstanceState, rootKey);
+        final PreferenceScreen screen = getPreferenceScreen();
+
+        if (screen == null) {
+            return;
+        }
+
         for (int i = 0; i < CHANGE_LAYOUT_KEYS.length; i++) {
             Preference preference = findPreference(CHANGE_LAYOUT_KEYS[i]);
             if (preference != null){
                 preference.setLayoutResource(LAYOUTS_N[i]);
             }
         }
-        for (int i = 0; i < CHANGE_LAYOUT_AND_ORDER_KEYS.length; i++) {
-            Preference preference = findPreference(CHANGE_LAYOUT_AND_ORDER_KEYS[i]);
+        if(getContext().getResources().getString(R.string.ds_ia) == "yourkeyhere"){
+            final int count = screen.getPreferenceCount();
+            for (int i = 0; i < count; i++) {
+                final Preference preference = screen.getPreference(i);
+                String key = preference.getKey();
+                if (preference == null) {
+                    break;
+                }
+                for (int n = 0; n< CHANGE_LAYOUT_AND_ORDER_KEYS.length; n++){
+                    if(key.contains(CHANGE_LAYOUT_AND_ORDER_KEYS[n])){
+                        preference.setLayoutResource(R.layout.rdnt_card_device);
+                        preference.setOrder(12);
+                    }
+                }
+            }
+        } else{
+            Preference preference = findPreference(getContext().getResources().getString(R.string.ds_ia));
             if (preference != null){
                 preference.setLayoutResource(R.layout.rdnt_card_device);
                 preference.setOrder(12);
             }
         }
+
     }
 
     @Override

BlissRoms/platform_packages_apps_Settings

Change-Id: I4068c95c51ebd98b08888ac868d8a1855bf77040
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -78,14 +78,15 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private ActivityEmbeddingController mActivityEmbeddingController;
 
     public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
-        "dashboard_tile_pref_com.oneplus.extras.DeviceSettings",
-        "dashboard_tile_pref_org.lineageos.settings.device",
-        "dashboard_tile_pref_org.lineageos.settings",
-        "dashboard_tile_pref_org.omnirom.devices",
-        "dashboard_tile_pref_org.omnirom.device",
-        "dashboard_tile_pref_com.poco.parts",
-        "dashboard_tile_pref_com.xiaomi.parts",
-        "dashboard_tile_pref_com.asus.zenparts"
+        "oneplus",
+        "realme",
+        "lineage",
+        "omni",
+        "phh",
+        "poco",
+        "xiaomi",
+        "asus",
+        "moto"
     };
 
     public static final String[] CHANGE_LAYOUT_KEYS = {
@@ -250,19 +251,41 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         if (Flags.homepageRevamp()) {
             return;
         }
+        final PreferenceScreen screen = getPreferenceScreen();
+
+        if (screen == null) {
+            return;
+        }
+
         for (int i = 0; i < CHANGE_LAYOUT_KEYS.length; i++) {
             Preference preference = findPreference(CHANGE_LAYOUT_KEYS[i]);
             if (preference != null){
                 preference.setLayoutResource(LAYOUTS_N[i]);
             }
         }
-        for (int i = 0; i < CHANGE_LAYOUT_AND_ORDER_KEYS.length; i++) {
-            Preference preference = findPreference(CHANGE_LAYOUT_AND_ORDER_KEYS[i]);
+        if(getContext().getResources().getString(R.string.ds_ia) == "yourkeyhere"){
+            final int count = screen.getPreferenceCount();
+            for (int i = 0; i < count; i++) {
+                final Preference preference = screen.getPreference(i);
+                String key = preference.getKey();
+                if (preference == null) {
+                    break;
+                }
+                for (int n = 0; n< CHANGE_LAYOUT_AND_ORDER_KEYS.length; n++){
+                    if(key.contains(CHANGE_LAYOUT_AND_ORDER_KEYS[n])){
+                        preference.setLayoutResource(R.layout.bliss_card_device);
+                        preference.setOrder(12);
+                    }
+                }
+            }
+        } else{
+            Preference preference = findPreference(getContext().getResources().getString(R.string.ds_ia));
             if (preference != null){
                 preference.setLayoutResource(R.layout.bliss_card_device);
                 preference.setOrder(12);
             }
         }
+
     }
 
     @Override