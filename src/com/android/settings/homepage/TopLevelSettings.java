ProjectRadiant/packages_apps_Settings

Settings: FINALLY fix DS IA detection

src/com/android/settings/homepage/TopLevelSettings.java
@@ -45,17 +45,6 @@ public class TopLevelSettings extends DashboardFragment implements
 
     private static final String TAG = "TopLevelSettings";
 
-    public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
-        "oneplus",
-        "realme",
-        "lineage",
-        "omni",
-        "phh",
-        "poco",
-        "xiaomi",
-        "asus",
-        "moto"
-    };
 
     public static final String[] CHANGE_LAYOUT_KEYS = {
         "top_level_google",
@@ -128,6 +117,19 @@ public class TopLevelSettings extends DashboardFragment implements
         super.onCreatePreferences(savedInstanceState, rootKey);
         final PreferenceScreen screen = getPreferenceScreen();
 
+        final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
+            "oneplus",
+            "realme",
+            "lineage",
+            "omni",
+            "phh",
+            "poco",
+            "xiaomi",
+            "asus",
+            "moto",
+            getContext().getResources().getString(R.string.ds_ia)
+        };
+
         if (screen == null) {
             return;
         }
@@ -138,26 +140,19 @@ public class TopLevelSettings extends DashboardFragment implements
                 preference.setLayoutResource(LAYOUTS_N[i]);
             }
         }
-        if(getContext().getResources().getString(R.string.ds_ia) == "yourkeyhere"){
-            final int count = screen.getPreferenceCount();
-            for (int i = 0; i < count; i++) {
-                final Preference preference = screen.getPreference(i);
-                String key = preference.getKey();
-                if (preference == null) {
-                    break;
-                }
-                for (int n = 0; n< CHANGE_LAYOUT_AND_ORDER_KEYS.length; n++){
-                    if(key.contains(CHANGE_LAYOUT_AND_ORDER_KEYS[n])){
-                        preference.setLayoutResource(R.layout.rdnt_card_device);
-                        preference.setOrder(12);
-                    }
-                }
+        
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            final Preference preference = screen.getPreference(i);
+            String key = preference.getKey();
+            if (preference == null) {
+                break;
             }
-        } else{
-            Preference preference = findPreference(getContext().getResources().getString(R.string.ds_ia));
-            if (preference != null){
-                preference.setLayoutResource(R.layout.rdnt_card_device);
-                preference.setOrder(12);
+            for (int n = 0; n< CHANGE_LAYOUT_AND_ORDER_KEYS.length; n++){
+                if(key.contains(CHANGE_LAYOUT_AND_ORDER_KEYS[n].toLowerCase())){
+                    preference.setLayoutResource(R.layout.rdnt_card_device);
+                    preference.setOrder(12);
+                }
             }
         }
 

BlissRoms/platform_packages_apps_Settings

Change-Id: I5cd912456fc54d81ffd58a1809d8472dcc1c8d23
Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

src/com/android/settings/homepage/TopLevelSettings.java
@@ -77,17 +77,6 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
     private boolean mFirstStarted = true;
     private ActivityEmbeddingController mActivityEmbeddingController;
 
-    public static final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
-        "oneplus",
-        "realme",
-        "lineage",
-        "omni",
-        "phh",
-        "poco",
-        "xiaomi",
-        "asus",
-        "moto"
-    };
 
     public static final String[] CHANGE_LAYOUT_KEYS = {
         "top_level_google",
@@ -253,6 +242,19 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
         }
         final PreferenceScreen screen = getPreferenceScreen();
 
+        final String[] CHANGE_LAYOUT_AND_ORDER_KEYS = {
+            "oneplus",
+            "realme",
+            "lineage",
+            "omni",
+            "phh",
+            "poco",
+            "xiaomi",
+            "asus",
+            "moto",
+            getContext().getResources().getString(R.string.ds_ia)
+        };
+
         if (screen == null) {
             return;
         }
@@ -263,26 +265,19 @@ public class TopLevelSettings extends DashboardFragment implements SplitLayoutLi
                 preference.setLayoutResource(LAYOUTS_N[i]);
             }
         }
-        if(getContext().getResources().getString(R.string.ds_ia) == "yourkeyhere"){
-            final int count = screen.getPreferenceCount();
-            for (int i = 0; i < count; i++) {
-                final Preference preference = screen.getPreference(i);
-                String key = preference.getKey();
-                if (preference == null) {
-                    break;
-                }
-                for (int n = 0; n< CHANGE_LAYOUT_AND_ORDER_KEYS.length; n++){
-                    if(key.contains(CHANGE_LAYOUT_AND_ORDER_KEYS[n])){
-                        preference.setLayoutResource(R.layout.bliss_card_device);
-                        preference.setOrder(12);
-                    }
-                }
+        
+        final int count = screen.getPreferenceCount();
+        for (int i = 0; i < count; i++) {
+            final Preference preference = screen.getPreference(i);
+            String key = preference.getKey();
+            if (preference == null) {
+                break;
             }
-        } else{
-            Preference preference = findPreference(getContext().getResources().getString(R.string.ds_ia));
-            if (preference != null){
-                preference.setLayoutResource(R.layout.bliss_card_device);
-                preference.setOrder(12);
+            for (int n = 0; n< CHANGE_LAYOUT_AND_ORDER_KEYS.length; n++){
+                if(key.contains(CHANGE_LAYOUT_AND_ORDER_KEYS[n].toLowerCase())){
+                    preference.setLayoutResource(R.layout.bliss_card_device);
+                    preference.setOrder(12);
+                }
             }
         }
 