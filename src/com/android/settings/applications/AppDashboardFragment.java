56f311cb49c794da232a8b4f0df9b456f0a76da8
Dhina17 <dhinalogu@gmail.com>
2024-11-29 16:44:27 +0530

    Settings: Move applock to apps section
    security advance section is too hidden
    so that users even don't know about applock.
    
    Change-Id: Iaaa2fd17befd915df485e16461abcc8ffc0f7191
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/applications/AppDashboardFragment.java
+++ src/com/android/settings/applications/AppDashboardFragment.java
@@ -28,8 +28,10 @@ import com.android.settings.R;
 import com.android.settings.applications.appcompat.UserAspectRatioAppsPreferenceController;
 import com.android.settings.dashboard.DashboardFragment;
 import com.android.settings.search.BaseSearchIndexProvider;
+import com.android.settings.security.applock.AppLockSettingsPreferenceController;
 import com.android.settings.widget.PreferenceCategoryController;
 import com.android.settingslib.core.AbstractPreferenceController;
+import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.search.SearchIndexable;
 
 import java.util.ArrayList;
@@ -44,9 +46,11 @@ public class AppDashboardFragment extends DashboardFragment {
     private static final String TAG = "AppDashboardFragment";
     private static final String ADVANCED_CATEGORY_KEY = "advanced_category";
     private static final String ASPECT_RATIO_PREF_KEY = "aspect_ratio_apps";
+    private static final String APP_LOCK_PREF_KEY = "app_lock";
     private AppsPreferenceController mAppsPreferenceController;
 
-    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context) {
+    private static List<AbstractPreferenceController> buildPreferenceControllers(Context context,
+            DashboardFragment host, Lifecycle lifecycle) {
         final List<AbstractPreferenceController> controllers = new ArrayList<>();
         controllers.add(new AppsPreferenceController(context));
 
@@ -56,6 +60,8 @@ public class AppDashboardFragment extends DashboardFragment {
                 new AdvancedAppsPreferenceCategoryController(context, ADVANCED_CATEGORY_KEY);
         advancedCategoryController.setChildren(List.of(aspectRatioAppsPreferenceController));
         controllers.add(advancedCategoryController);
+        controllers.add(new AppLockSettingsPreferenceController(
+                context, APP_LOCK_PREF_KEY, host, lifecycle));
         return controllers;
     }
 
@@ -103,7 +109,7 @@ public class AppDashboardFragment extends DashboardFragment {
 
     @Override
     protected List<AbstractPreferenceController> createPreferenceControllers(Context context) {
-        return buildPreferenceControllers(context);
+        return buildPreferenceControllers(context, this /* host */, getSettingsLifecycle());
     }
 
     public static final BaseSearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
@@ -119,7 +125,7 @@ public class AppDashboardFragment extends DashboardFragment {
                 @Override
                 public List<AbstractPreferenceController> createPreferenceControllers(
                         Context context) {
-                    return buildPreferenceControllers(context);
+                    return buildPreferenceControllers(context, null /* host */, null);
                 }
             };
 }
