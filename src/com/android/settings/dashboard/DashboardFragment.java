tavukkdoner/android_packages_apps_Settings

Fixup extras/parts/settings key injection
Needed after "Settings: Re-organize top level preferences".

src/com/android/settings/dashboard/DashboardFragment.java
@@ -78,7 +78,10 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     private static final long TIMEOUT_MILLIS = 50L;
 
     private static final List<String> ACCOUNT_INJECTED_KEYS = Arrays.asList(
-        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity",
+        "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity"
+    );
+
+    private static final List<String> PERSONALIZATION_INJECTED_KEYS = Arrays.asList(
         "crdroid_device_parts_settings"
     );
 
@@ -89,6 +92,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
 
     private static final ArrayMap<String, Integer> KEY_ORDER = new ArrayMap<>();
     static {
+        // Snuggle up just under crDroid Settings at "-60" above, and something else at -50 below
+        KEY_ORDER.put("crdroid_device_parts_settings", -55);
         // We have "Passwords, passkeys & accounts with order "-10" above
         KEY_ORDER.put("top_level_wellbeing", -5);
         KEY_ORDER.put("top_level_google", 0);
@@ -580,6 +585,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                         group = screen.findPreference(tile.getGroupKey());
                     } else if (ACCOUNT_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_account_category");
+                    } else if (PERSONALIZATION_INJECTED_KEYS.contains(key)) {
+                        group = screen.findPreference("top_level_personalize_category");
                     } else if (SECURITY_PRIVACY_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_security_privacy_category");
                     } else {

BlissRoms/platform_packages_apps_Settings

Signed-off-by: Jackeagle <jackeagle102@gmail.com>

src/com/android/settings/dashboard/DashboardFragment.java
@@ -82,12 +82,15 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
     private static final long TIMEOUT_MILLIS = 50L;
 
     private static final List<String> ACCOUNT_INJECTED_KEYS = Arrays.asList(
-        "top_level_google",
-        "bliss_device_parts_settings"
+        "top_level_google"
     );
 
     private static final List<String> SYSTEM_INFO_INJECTED_KEYS = Arrays.asList(
         "dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity"
+    );
+
+    private static final List<String> PERSONALIZATION_INJECTED_KEYS = Arrays.asList(
+        "bliss_device_parts_settings"
     );
 
     private static final List<String> SECURITY_PRIVACY_INJECTED_KEYS = Arrays.asList(
@@ -99,6 +102,9 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
         // We have "Storage" with order "-50" above
         KEY_ORDER.put("dashboard_tile_pref_com.google.android.gms.backup.component.BackupOrRestoreSettingsActivity", -45);
         // We have "Battery" with order "-30" below
+
+        // Snuggle up just under Blissify at "-60" above, and something else at -50 below
+        KEY_ORDER.put("bliss_device_parts_settings", -55);
     }
 
     @VisibleForTesting
@@ -675,6 +681,8 @@ public abstract class DashboardFragment extends SettingsPreferenceFragment
                         group = screen.findPreference("top_level_account_category");
                     } else if (SYSTEM_INFO_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_system_info_category");
+                    } else if (PERSONALIZATION_INJECTED_KEYS.contains(key)) {
+                        group = screen.findPreference("top_level_personalize_category");
                     } else if (SECURITY_PRIVACY_INJECTED_KEYS.contains(key)) {
                         group = screen.findPreference("top_level_security_privacy_category");
                     } else {