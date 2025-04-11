tavukkdoner/android_packages_apps_Settings

add per-connection Wi-Fi MAC address randomization option [1/2]
fixup! add per-connection Wi-Fi MAC address randomization option

src/com/android/settings/wifi/WifiConfigController.java
@@ -73,6 +73,7 @@ import com.android.settings.R;
 import com.android.settings.network.SubscriptionUtil;
 import com.android.settings.utils.AndroidKeystoreAliasLoader;
 import com.android.settings.wifi.details2.WifiPrivacyPreferenceController;
+import com.android.settings.wifi.details2.WifiPrivacyPreferenceController2;
 import com.android.settings.wifi.dpp.WifiDppUtils;
 import com.android.settingslib.Utils;
 import com.android.settingslib.utils.ThreadUtils;
@@ -151,9 +152,10 @@ public class WifiConfigController implements TextWatcher,
         UNDESIRED_CERTIFICATE_MACRANDSAPSECRET
     };
 
-    // Should be the same index value as wifi_privacy_entries in arrays.xml
-    @VisibleForTesting static final int PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC = 0;
-    @VisibleForTesting static final int PRIVACY_SPINNER_INDEX_DEVICE_MAC = 1;
+    // Should be the same index value as wifi_privacy_entries_ext in arrays.xml
+    public static final int PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC = 0;
+    public static final int PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC = 1;
+    public static final int PRIVACY_PREF_INDEX_DEVICE_MAC = 2;
 
     // Should be the same index value as wifi_dhcp_entries in arrays.xml
     @VisibleForTesting static final int DHCP_SPINNER_INDEX_SEND_DHCP_HOST_NAME_ENABLE = 0;
@@ -326,9 +328,9 @@ public class WifiConfigController implements TextWatcher,
                         ? HIDDEN_NETWORK
                         : NOT_HIDDEN_NETWORK);
 
-                mPrivacySettingsSpinner.setSelection(
-                        config.macRandomizationSetting == WifiConfiguration.RANDOMIZATION_PERSISTENT
-                        ? PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC : PRIVACY_SPINNER_INDEX_DEVICE_MAC);
+                int selection = WifiPrivacyPreferenceController2
+                        .translateMacRandomizedValueToPrefValue(config.macRandomizationSetting);
+                mPrivacySettingsSpinner.setSelection(selection);
 
                 mDhcpSettingsSpinner.setSelection(
                         config.isSendDhcpHostnameEnabled()
@@ -848,10 +850,8 @@ public class WifiConfigController implements TextWatcher,
         }
 
         if (mPrivacySettingsSpinner != null) {
-            config.macRandomizationSetting = mPrivacySettingsSpinner.getSelectedItemPosition()
-                    == PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC
-                    ? WifiConfiguration.RANDOMIZATION_PERSISTENT
-                    : WifiConfiguration.RANDOMIZATION_NONE;
+            config.macRandomizationSetting = WifiPrivacyPreferenceController2
+                    .translatePrefValueToMacRandomizedValue(mPrivacySettingsSpinner.getSelectedItemPosition());
         }
 
         if (mDhcpSettingsSpinner != null) {

src/com/android/settings/wifi/details2/WifiPrivacyPreferenceController2.java
@@ -18,6 +18,7 @@ package com.android.settings.wifi.details2;
 
 import android.content.Context;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -28,6 +29,10 @@ import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 import com.android.wifi.flags.Flags;
 import com.android.wifitrackerlib.WifiEntry;
+
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_DEVICE_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC;
 
 /**
  * A controller that controls whether the Wi-Fi network is mac randomized or not.
@@ -94,9 +99,6 @@ public class WifiPrivacyPreferenceController2 extends BasePreferenceController i
         return mWifiEntry.getPrivacy();
     }
 
-    private static final int PREF_RANDOMIZATION_PERSISTENT = 0;
-    private static final int PREF_RANDOMIZATION_NONE = 1;
-
     /**
      * Returns preference index value.
      *
@@ -104,8 +106,13 @@ public class WifiPrivacyPreferenceController2 extends BasePreferenceController i
      * @return index value of preference
      */
     public static int translateMacRandomizedValueToPrefValue(int macRandomized) {
-        return (macRandomized == WifiEntry.PRIVACY_RANDOMIZED_MAC)
-            ? PREF_RANDOMIZATION_PERSISTENT : PREF_RANDOMIZATION_NONE;
+        Log.d("WifiMacRnd", "translateMacRandomizedValueToPrefValue called from", new Throwable());
+        return switch (macRandomized) {
+            case WifiEntry.PRIVACY_RANDOMIZATION_ALWAYS -> PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC;
+            case WifiEntry.PRIVACY_RANDOMIZED_MAC -> PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC;
+            case WifiEntry.PRIVACY_DEVICE_MAC -> PRIVACY_PREF_INDEX_DEVICE_MAC;
+            default -> PRIVACY_PREF_INDEX_DEVICE_MAC;
+        };
     }
 
     /**
@@ -115,8 +122,13 @@ public class WifiPrivacyPreferenceController2 extends BasePreferenceController i
      * @return mac randomized value
      */
     public static int translatePrefValueToMacRandomizedValue(int prefMacRandomized) {
-        return (prefMacRandomized == PREF_RANDOMIZATION_PERSISTENT)
-            ? WifiEntry.PRIVACY_RANDOMIZED_MAC : WifiEntry.PRIVACY_DEVICE_MAC;
+        Log.d("WifiMacRnd", "translatePrefValueToMacRandomizedValue called from", new Throwable());
+        return switch (prefMacRandomized) {
+            case PRIVACY_PREF_INDEX_DEVICE_MAC -> WifiEntry.PRIVACY_DEVICE_MAC;
+            case PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC -> WifiEntry.PRIVACY_RANDOMIZED_MAC;
+            case PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC -> WifiEntry.PRIVACY_RANDOMIZATION_ALWAYS;
+            default -> WifiEntry.PRIVACY_RANDOMIZATION_ALWAYS;
+        };
     }
 
     private void updateSummary(ListPreference preference, int macRandomized) {

tests/robotests/src/com/android/settings/wifi/WifiConfigControllerTest.java
@@ -16,11 +16,12 @@
 
 package com.android.settings.wifi;
 
-import static com.android.settings.wifi.WifiConfigController.PRIVACY_SPINNER_INDEX_DEVICE_MAC;
-import static com.android.settings.wifi.WifiConfigController.PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_DEVICE_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC;
 import static com.android.settings.wifi.WifiConfigController.DHCP_SPINNER_INDEX_SEND_DHCP_HOST_NAME_ENABLE;
 import static com.android.settings.wifi.WifiConfigController.DHCP_SPINNER_INDEX_SEND_DHCP_HOST_NAME_DISABLE;
 
+import static com.android.settings.wifi.details2.WifiPrivacyPreferenceController2.translateMacRandomizedValueToPrefValue;
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.Mockito.mock;
@@ -407,7 +408,7 @@ public class WifiConfigControllerTest {
 
         assertThat(privacySetting.getVisibility()).isEqualTo(View.VISIBLE);
         assertThat(privacySetting.getSelectedItemPosition()).isEqualTo(
-                PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC);
+                PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC);
     }
 
     @Test
@@ -432,8 +433,7 @@ public class WifiConfigControllerTest {
 
         assertThat(privacySetting.getVisibility()).isEqualTo(View.VISIBLE);
         assertThat(privacySetting.getSelectedItemPosition()).isEqualTo(
-                macRandomizedValue == WifiConfiguration.RANDOMIZATION_PERSISTENT
-                ? PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC : PRIVACY_SPINNER_INDEX_DEVICE_MAC);
+                translateMacRandomizedValueToPrefValue(macRandomizedValue));
     }
 
     @Test
@@ -446,7 +446,7 @@ public class WifiConfigControllerTest {
     @Test
     public void saveMacRandomizedValue_ChangedToDeviceMac_shouldGetNone() {
         final Spinner privacySetting = mView.findViewById(R.id.privacy_settings);
-        privacySetting.setSelection(PRIVACY_SPINNER_INDEX_DEVICE_MAC);
+        privacySetting.setSelection(PRIVACY_PREF_INDEX_DEVICE_MAC);
 
         WifiConfiguration config = mController.getConfig();
         assertThat(config.macRandomizationSetting).isEqualTo(WifiConfiguration.RANDOMIZATION_NONE);

BlissRoms/platform_packages_apps_Settings

Signed-off-by: Jackeagle <jackeagle102@gmail.com>

src/com/android/settings/wifi/WifiConfigController.java
@@ -73,6 +73,7 @@ import com.android.settings.R;
 import com.android.settings.network.SubscriptionUtil;
 import com.android.settings.utils.AndroidKeystoreAliasLoader;
 import com.android.settings.wifi.details2.WifiPrivacyPreferenceController;
+import com.android.settings.wifi.details2.WifiPrivacyPreferenceController2;
 import com.android.settings.wifi.dpp.WifiDppUtils;
 import com.android.settingslib.Utils;
 import com.android.settingslib.utils.ThreadUtils;
@@ -151,9 +152,10 @@ public class WifiConfigController implements TextWatcher,
         UNDESIRED_CERTIFICATE_MACRANDSAPSECRET
     };
 
-    // Should be the same index value as wifi_privacy_entries in arrays.xml
-    @VisibleForTesting static final int PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC = 0;
-    @VisibleForTesting static final int PRIVACY_SPINNER_INDEX_DEVICE_MAC = 1;
+    // Should be the same index value as wifi_privacy_entries_ext in arrays.xml
+    public static final int PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC = 0;
+    public static final int PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC = 1;
+    public static final int PRIVACY_PREF_INDEX_DEVICE_MAC = 2;
 
     // Should be the same index value as wifi_dhcp_entries in arrays.xml
     @VisibleForTesting static final int DHCP_SPINNER_INDEX_SEND_DHCP_HOST_NAME_ENABLE = 0;
@@ -326,9 +328,9 @@ public class WifiConfigController implements TextWatcher,
                         ? HIDDEN_NETWORK
                         : NOT_HIDDEN_NETWORK);
 
-                mPrivacySettingsSpinner.setSelection(
-                        config.macRandomizationSetting == WifiConfiguration.RANDOMIZATION_PERSISTENT
-                        ? PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC : PRIVACY_SPINNER_INDEX_DEVICE_MAC);
+                int selection = WifiPrivacyPreferenceController2
+                        .translateWifiEntryPrivacyToPrefValue(config.macRandomizationSetting);
+                mPrivacySettingsSpinner.setSelection(selection);
 
                 mDhcpSettingsSpinner.setSelection(
                         config.isSendDhcpHostnameEnabled()
@@ -848,10 +850,8 @@ public class WifiConfigController implements TextWatcher,
         }
 
         if (mPrivacySettingsSpinner != null) {
-            config.macRandomizationSetting = mPrivacySettingsSpinner.getSelectedItemPosition()
-                    == PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC
-                    ? WifiConfiguration.RANDOMIZATION_PERSISTENT
-                    : WifiConfiguration.RANDOMIZATION_NONE;
+            config.macRandomizationSetting = WifiPrivacyPreferenceController2
+                    .translatePrefValueToWifiConfigSetting(mPrivacySettingsSpinner.getSelectedItemPosition());
         }
 
         if (mDhcpSettingsSpinner != null) {

src/com/android/settings/wifi/details2/WifiPrivacyPreferenceController2.java
@@ -19,6 +19,7 @@ package com.android.settings.wifi.details2;
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.VisibleForTesting;
@@ -29,6 +30,10 @@ import com.android.settings.R;
 import com.android.settings.core.BasePreferenceController;
 import com.android.wifi.flags.Flags;
 import com.android.wifitrackerlib.WifiEntry;
+
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_DEVICE_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC;
 
 /**
  * A controller that controls whether the Wi-Fi network is mac randomized or not.
@@ -95,9 +100,6 @@ public class WifiPrivacyPreferenceController2 extends BasePreferenceController i
         return mWifiEntry.getPrivacy();
     }
 
-    private static final int PREF_RANDOMIZATION_PERSISTENT = 0;
-    private static final int PREF_RANDOMIZATION_NONE = 1;
-
     /**
      * Translates a WifiEntry.Privacy value to the matching preference index value.
      *
@@ -105,8 +107,13 @@ public class WifiPrivacyPreferenceController2 extends BasePreferenceController i
      * @return index value of preference
      */
     public static int translateWifiEntryPrivacyToPrefValue(@WifiEntry.Privacy int privacy) {
-        return (privacy == WifiEntry.PRIVACY_RANDOMIZED_MAC)
-            ? PREF_RANDOMIZATION_PERSISTENT : PREF_RANDOMIZATION_NONE;
+        Log.d("WifiMacRnd", "translateMacRandomizedValueToPrefValue called from", new Throwable());
+        return switch (privacy) {
+            case WifiEntry.PRIVACY_RANDOMIZATION_ALWAYS -> PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC;
+            case WifiEntry.PRIVACY_RANDOMIZED_MAC -> PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC;
+            case WifiEntry.PRIVACY_DEVICE_MAC -> PRIVACY_PREF_INDEX_DEVICE_MAC;
+            default -> PRIVACY_PREF_INDEX_DEVICE_MAC;
+        };
     }
 
     /**
@@ -116,8 +123,13 @@ public class WifiPrivacyPreferenceController2 extends BasePreferenceController i
      * @return WifiConfiguration.MacRandomizationSetting value
      */
     public static int translatePrefValueToWifiConfigSetting(int prefMacRandomized) {
-        return (prefMacRandomized == PREF_RANDOMIZATION_PERSISTENT)
-            ? WifiConfiguration.RANDOMIZATION_AUTO : WifiConfiguration.RANDOMIZATION_NONE;
+        Log.d("WifiMacRnd", "translatePrefValueToWifiConfigSetting called from", new Throwable());
+        return switch (prefMacRandomized) {
+            case PRIVACY_PREF_INDEX_DEVICE_MAC -> WifiConfiguration.RANDOMIZATION_NONE;
+            case PRIVACY_PREF_INDEX_PER_NETWORK_RANDOMIZED_MAC -> WifiConfiguration.RANDOMIZATION_PERSISTENT;
+            case PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC -> WifiConfiguration.RANDOMIZATION_ALWAYS;
+            default -> WifiConfiguration.RANDOMIZATION_ALWAYS;
+        };
     }
 
     private void updateSummary(ListPreference preference, int macRandomized) {

tests/robotests/src/com/android/settings/wifi/WifiConfigControllerTest.java
@@ -16,11 +16,12 @@
 
 package com.android.settings.wifi;
 
-import static com.android.settings.wifi.WifiConfigController.PRIVACY_SPINNER_INDEX_DEVICE_MAC;
-import static com.android.settings.wifi.WifiConfigController.PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_DEVICE_MAC;
+import static com.android.settings.wifi.WifiConfigController.PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC;
 import static com.android.settings.wifi.WifiConfigController.DHCP_SPINNER_INDEX_SEND_DHCP_HOST_NAME_ENABLE;
 import static com.android.settings.wifi.WifiConfigController.DHCP_SPINNER_INDEX_SEND_DHCP_HOST_NAME_DISABLE;
 
+import static com.android.settings.wifi.details2.WifiPrivacyPreferenceController2.translateMacRandomizedValueToPrefValue;
 import static com.google.common.truth.Truth.assertThat;
 
 import static org.mockito.Mockito.mock;
@@ -407,7 +408,7 @@ public class WifiConfigControllerTest {
 
         assertThat(privacySetting.getVisibility()).isEqualTo(View.VISIBLE);
         assertThat(privacySetting.getSelectedItemPosition()).isEqualTo(
-                PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC);
+                PRIVACY_PREF_INDEX_PER_CONNECTION_RANDOMIZED_MAC);
     }
 
     @Test
@@ -432,8 +433,7 @@ public class WifiConfigControllerTest {
 
         assertThat(privacySetting.getVisibility()).isEqualTo(View.VISIBLE);
         assertThat(privacySetting.getSelectedItemPosition()).isEqualTo(
-                macRandomizedValue == WifiConfiguration.RANDOMIZATION_PERSISTENT
-                ? PRIVACY_SPINNER_INDEX_RANDOMIZED_MAC : PRIVACY_SPINNER_INDEX_DEVICE_MAC);
+                translateWifiEntryPrivacyToPrefValue(macRandomizedValue));
     }
 
     @Test
@@ -446,7 +446,7 @@ public class WifiConfigControllerTest {
     @Test
     public void saveMacRandomizedValue_ChangedToDeviceMac_shouldGetNone() {
         final Spinner privacySetting = mView.findViewById(R.id.privacy_settings);
-        privacySetting.setSelection(PRIVACY_SPINNER_INDEX_DEVICE_MAC);
+        privacySetting.setSelection(PRIVACY_PREF_INDEX_DEVICE_MAC);
 
         WifiConfiguration config = mController.getConfig();
         assertThat(config.macRandomizationSetting).isEqualTo(WifiConfiguration.RANDOMIZATION_NONE);