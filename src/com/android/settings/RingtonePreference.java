crdroidx/android_packages_apps_Settings

Settings: Safeguard ringtone pref against faulty gapps
* Gapps which replaces AOSP sound picker and do not overlay config, crashes this preference.

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/RingtonePreference.java
@@ -78,8 +78,12 @@ public class RingtonePreference extends Preference {
                 true);
         mShowSilent = a.getBoolean(com.android.internal.R.styleable.RingtonePreference_showSilent,
                 true);
-        setIntent(new Intent(RingtoneManager.ACTION_RINGTONE_PICKER)
-                .setPackage(context.getString(R.string.config_sound_picker_package_name)));
+        String packageName = context.getString(R.string.config_sound_picker_package_name);
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        if (com.android.internal.util.crdroid.Utils.isPackageInstalled(context, packageName)) {
+            intent.setPackage(packageName);
+        }
+        setIntent(intent);
         setUserId(UserHandle.myUserId());
         a.recycle();
     }

BlissRoms/platform_packages_apps_Settings

Change-Id: Iad6054a453b903f2a2be18619414b83e707efb86
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

src/com/android/settings/RingtonePreference.java
@@ -74,8 +74,12 @@ public class RingtonePreference extends Preference {
                 true);
         mShowSilent = a.getBoolean(com.android.internal.R.styleable.RingtonePreference_showSilent,
                 true);
-        setIntent(new Intent(RingtoneManager.ACTION_RINGTONE_PICKER)
-                .setPackage(context.getString(R.string.config_sound_picker_package_name)));
+        String packageName = context.getString(R.string.config_sound_picker_package_name);
+        Intent intent = new Intent(RingtoneManager.ACTION_RINGTONE_PICKER);
+        if (com.android.internal.util.bliss.BlissUtils.isPackageInstalled(context, packageName)) {
+            intent.setPackage(packageName);
+        }
+        setIntent(intent);
         setUserId(UserHandle.myUserId());
         a.recycle();
     }