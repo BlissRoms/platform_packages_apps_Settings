alioth-aosp/android_packages_apps_Settings

Settings: Prompt SystemUI restart on toggling navbar
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -62,6 +62,8 @@ import com.android.settingslib.widget.CandidateInfo;
 import com.android.settingslib.widget.IllustrationPreference;
 import com.android.settingslib.widget.SelectorWithWidgetPreference;
 
+import com.crdroid.settings.utils.SystemUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -239,6 +241,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
             if (threeButtonNav != null) threeButtonNav.setEnabled(showing);
             if (twoButtonNav != null) twoButtonNav.setEnabled(showing);
             if (gesturalNav != null) gesturalNav.setEnabled(showing);
+            SystemUtils.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;

BlissRoms/platform_packages_apps_Settings

Change-Id: I3d0c4ae2ede6f3740ad3ccfc7bddf62520c56223
Signed-off-by: Jackeagle <jackeagle102@gmail.com>

src/com/android/settings/gestures/SystemNavigationGestureSettings.java
@@ -62,6 +62,8 @@ import com.android.settingslib.widget.CandidateInfo;
 import com.android.settingslib.widget.IllustrationPreference;
 import com.android.settingslib.widget.SelectorWithWidgetPreference;
 
+import org.blissroms.blissify.utils.SystemUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -230,6 +232,7 @@ public class SystemNavigationGestureSettings extends RadioButtonPickerFragment i
             if (threeButtonNav != null) threeButtonNav.setEnabled(showing);
             if (twoButtonNav != null) twoButtonNav.setEnabled(showing);
             if (gesturalNav != null) gesturalNav.setEnabled(showing);
+            SystemUtils.showSystemUiRestartDialog(getContext());
             return true;
         }
         return false;