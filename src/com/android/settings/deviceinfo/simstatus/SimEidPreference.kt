486416a16db862623bdc5c4f23df1cf325ae8d56
Dmitrii <bankersenator@gmail.com>
2025-12-07 18:49:39 +0000

    Revert "Modify the EID title"
    This reverts commit b240349cdbcc9668a7aa4e0d4d3657fae7c793f4.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>

--- src/com/android/settings/deviceinfo/simstatus/SimEidPreference.kt
+++ src/com/android/settings/deviceinfo/simstatus/SimEidPreference.kt
@@ -138,8 +138,8 @@ class SimEidPreference(private val context: Context) :
     private data class EidMetadata(val eid: String, val associatedSlotId: Int?)
 
     private fun EidMetadata?.getTitle(context: Context): String {
-        // Since there is the MEP feature, there is one EID item.
-        return context.getString(R.string.status_eid)
+        val slotId = this?.associatedSlotId ?: return context.getString(R.string.status_eid)
+        return context.getString(R.string.eid_multi_sim, slotId + 1)
     }
 
     companion object {
--- tests/unit/src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceTest.kt
+++ tests/unit/src/com/android/settings/deviceinfo/simstatus/SimEidPreferenceTest.kt
@@ -23,7 +23,6 @@ import android.telephony.TelephonyManager
 import android.telephony.euicc.EuiccManager
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.android.settings.R
 import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
@@ -104,11 +103,6 @@ class SimEidPreferenceTest {
     @Test
     fun getSummary_returnEid() {
         assertThat(preference.getSummary(context)).isEqualTo(EID)
-    }
-
-    @Test
-    fun getTitle_returnEid() {
-        assertThat(preference.getTitle(context)).isEqualTo(context.getString(R.string.status_eid))
     }
 
     companion object {
