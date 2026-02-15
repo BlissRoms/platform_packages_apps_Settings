ccfee056948de2ed277cafa551ec95906974868e
Pranav Vashi <neobuddy89@gmail.com>
2026-02-15 22:06:14 +0530

    Settings: Set proper initial value for multi media focus
    * Due to past commits, it is likely that this value is already set.
    * Let's check when creating preferences.
    * Also move settings under Audio sub-settings.
    
    [Frost: Adapt to use config_multi_audio_focus_enabled_default]
    
    Change-Id: I23640e9a107f382b2f1080fae9f0683a78d4b373
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/notification/SoundSettings.java
+++ src/com/android/settings/notification/SoundSettings.java
@@ -26,6 +26,7 @@ import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.os.UserHandle;
+import android.provider.Settings;
 import android.text.TextUtils;
 
 import androidx.annotation.NonNull;
@@ -49,6 +50,8 @@ import com.android.settingslib.core.instrumentation.Instrumentable;
 import com.android.settingslib.core.lifecycle.Lifecycle;
 import com.android.settingslib.preference.UtilsKt;
 import com.android.settingslib.search.SearchIndexable;
+
+import org.blissroms.blissify.preferences.SystemSettingSwitchPreference;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -127,6 +130,15 @@ public class SoundSettings extends DashboardFragment implements OnActivityResult
             }
             return null;
         });
+
+        boolean defaultMultiAudioFocus = android.media.audio.Flags.audioFocusDesktop()
+                && getContext().getResources().getBoolean(
+                        com.android.internal.R.bool.config_multi_audio_focus_enabled_default);
+        boolean mediaFocus = Settings.System.getIntForUser(getContext().getContentResolver(),
+                Settings.System.MULTI_AUDIO_FOCUS_ENABLED, defaultMultiAudioFocus ? 1 : 0, UserHandle.USER_CURRENT) != 0;
+        SystemSettingSwitchPreference mediaFocusPref =
+                (SystemSettingSwitchPreference) findPreference("multi_audio_focus_enabled");
+        mediaFocusPref.setChecked(mediaFocus);
     }
 
     @Override
