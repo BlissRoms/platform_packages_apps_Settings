4ee23a39f74cef0998c9f8d09f3d7bb948f5509e
Jackeagle <jackeagle102@gmail.com>
2026-07-18 07:51:26 -0400

    Settings: Remove LineageOS references
    Change-Id: I3b6356cf00e1b68cbe5542bc168161af66b1f450
    Signed-off-by: Jackeagle <jackeagle102@gmail.com>
    Signed-off-by: Jis G Jacob <studiokeys@blissroms.org>

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -71,13 +71,10 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
     override fun getPreferenceHierarchy(context: Context, coroutineScope: CoroutineScope) =
         preferenceHierarchy(context) {
             +FirmwareVersionDetailPreference()
-            +LineageVersionDetailPreference()
             +SecurityPatchLevelPreference()
-            +LineageVendorSecurityPatchLevelPreference()
             +MainlineModuleVersionPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
-            +LineageBuildDatePreference()
             +SimpleBuildNumberPreference()
         }
 
--- src/com/android/settings/deviceinfo/legal/LegalSettingsScreen.kt
+++ src/com/android/settings/deviceinfo/legal/LegalSettingsScreen.kt
@@ -61,11 +61,6 @@ open class LegalSettingsScreen : PreferenceScreenMixin {
                 R.string.license_title,
                 "android.settings.LICENSE",
             )
-            +LineageLegalPreference(
-                "lineage_license",
-                R.string.terms_purpose,
-                R.string.lineagelicense_title,
-            )
             +LegalPreference(
                 "terms",
                 R.string.terms_purpose,
